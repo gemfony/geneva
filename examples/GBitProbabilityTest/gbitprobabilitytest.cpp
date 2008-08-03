c872e78d386781b25d73c77bdb95b5ad6a36dca2
rberlich <Unknown>
2008-08-03 12:09:33 +0000

    First steps towards another test case

---
+++ examples/GBitProbabilityTest/gbitprobabilitytest.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file
+ */
+
+/* gbitprobabilitytest.cpp
+ *
+ * Copyright (C) 2004-2008 Dr. Ruediger Berlich
+ * Copyright (C) 2007-2008 Forschungszentrum Karlsruhe GmbH
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard header files go here
+#include <iostream>
+#include <cmath>
+#include <sstream>
+
+// Boost header files go here
+#include <boost/function.hpp>
+
+// GenEvA header files go here
+#include "GBit.hpp"
+#include "GLogger.hpp"
+#include "GenevaExceptions.hpp"
+
+
+using namespace boost;
+using namespace Gem;
+using namespace Gem::GenEvA;
+
+/*
+ * Th
+ */
+
+
+/************************************************************************************************/
+/**
+ * The main function
+ */
+int main(int argc, char **argv){
+
+
+	return 0;
+}
