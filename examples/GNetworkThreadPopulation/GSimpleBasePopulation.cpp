c6194ce2f68ebded29406b8ed8786069b162c0b5
rberlich <Unknown>
2008-08-30 12:03:39 +0000

    Preparation for a neural network example

---
+++ examples/GNetworkThreadPopulation/GSimpleBasePopulation.cpp
@@ -0,0 +1,128 @@
+/**
+ * @file GSimpleBasePopulation.cpp
+ */
+
+/* Copyright (C) 2004-2008 Dr. Ruediger Berlich
+ * Copyright (C) 2007-2008 Forschungszentrum Karlsruhe GmbH
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard header files go here
+#include <iostream>
+#include <cmath>
+#include <sstream>
+
+// Boost header files go here
+
+// GenEvA header files go here
+#include "GRandom.hpp"
+#include "GBasePopulation.hpp"
+#include "GDoubleCollection.hpp"
+#include "GParameterSet.hpp"
+#include "GDoubleGaussAdaptor.hpp"
+#include "GLogger.hpp"
+#include "GLogTargets.hpp"
+#include "GBoostThreadPopulation.hpp"
+
+// The individual that should be optimized
+// This is a simple parabola
+#include "GParabolaIndividual.hpp"
+
+// Declares a function to parse the command line
+#include "GCommandLineParser.hpp"
+
+using namespace Gem::GenEvA;
+using namespace Gem::Util;
+using namespace Gem::GLogFramework;
+
+/************************************************************************************************/
+/**
+ * The main function. We search for the minimum of a parabola. This example demonstrates the use
+ * of the GBasePopulation class or (at your choice) of the GBoostThreadPopulation class. Note that
+ * a number of command line options are available. Call the executable with the "-h" switch
+ * to get an overview.
+ */
+int main(int argc, char **argv){
+	 std::size_t parabolaDimension, nPopThreads;
+	 std::size_t populationSize, nParents;
+	 double parabolaMin, parabolaMax;
+	 boost::uint16_t nProducerThreads;
+	 boost::uint32_t maxGenerations, reportGeneration;
+	 long maxMinutes;
+	 bool verbose;
+	 recoScheme rScheme;
+
+	// Parse the command line
+	if(!parseCommandLine(argc, argv,
+		  			     parabolaDimension,
+						 parabolaMin,
+						 parabolaMax,
+						 nProducerThreads,
+						 nPopThreads,
+						 populationSize,
+						 nParents,
+						 maxGenerations,
+						 maxMinutes,
+						 reportGeneration,
+						 rScheme,
+						 verbose))
+	{ std::terminate(); }
+
+	// Add some log levels to the logger
+	LOGGER.addLogLevel(Gem::GLogFramework::CRITICAL);
+	LOGGER.addLogLevel(Gem::GLogFramework::WARNING);
+	LOGGER.addLogLevel(Gem::GLogFramework::INFORMATIONAL);
+	LOGGER.addLogLevel(Gem::GLogFramework::PROGRESS);
+
+	// Add log targets to the system
+	LOGGER.addTarget(boost::shared_ptr<GBaseLogTarget>(new GDiskLogger("GSimpleBasePopulation.log")));
+	LOGGER.addTarget(boost::shared_ptr<GBaseLogTarget>(new GConsoleLogger()));
+
+	// Random numbers are our most valuable good. Set the number of threads
+	GRANDOMFACTORY.setNProducerThreads(nProducerThreads);
+
+	// Set up a single parabola individual
+	boost::shared_ptr<GParabolaIndividual>
+		parabolaIndividual(new GParabolaIndividual(parabolaDimension, parabolaMin, parabolaMax));
+
+	// Now we've got our first individual and can create a population.
+	// You can choose between a simple, non-parallel population and a
+	// multi-threaded population.
+
+	// Uncomment the next line and comment out the GBoostThreadPopulation lines
+	// to get the slower, serial execution.
+
+	// GBasePopulation pop;
+
+	GBoostThreadPopulation pop;
+	pop.setNThreads(nPopThreads);
+
+	pop.append(parabolaIndividual);
+
+	// Specify some population settings
+	pop.setPopulationSize(populationSize,nParents);
+	pop.setMaxGeneration(maxGenerations);
+	pop.setMaxTime(boost::posix_time::minutes(maxMinutes)); // Calculation should be finished after 5 minutes
+	pop.setReportGeneration(reportGeneration); // Emit information during every generation
+	pop.setRecombinationMethod(rScheme); // The best parents have higher chances of survival
+
+	// Do the actual optimization
+	pop.optimize();
+
+	std::cout << "Done ..." << std::endl;
+
+	return 0;
+}
