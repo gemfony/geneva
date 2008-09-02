36fbe657883219de42cc59c0f1fd5745cc81f3f2
rberlich <Unknown>
2008-09-02 21:36:27 +0000

    Further initial work on the test infrastructure

---
+++ tests/UnitTests/GObject/GObject_test.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file GObject_test.cpp
+ */
+
+/* Copyright (C) 2004-2008 Dr. Ruediger Berlich
+ * Copyright (C) 2007-2008 Forschungszentrum Karlsruhe GmbH
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard header files go here
+
+// Boost header files go here
+
+// Geneva header files go here
