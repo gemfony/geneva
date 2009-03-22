191d3571dcf06c85c07620d60d1f68c58f86b73c
rberlich <Unknown>
2009-03-22 14:16:45 +0000

    Work towards "standardized" communication with external evaluation programs

---
+++ include/GCharParameter.hpp
@@ -0,0 +1,57 @@
+/**
+ * @file GCharParameter.hpp
+ */
+
+/* Copyright (C) 2009 Dr. Ruediger Berlich
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License,
+ * or, at your option, under the terms of version 2 of the GNU General Public
+ * License, as published by the Free Software Foundation.
+ *
+ * NOTE THAT THIS FORM OF DUAL-LICENSING DOES NOT APPLY TO ANY OTHER FILES
+ * OF THE GENEVA LIBRARY, UNLESS THIS IS EXPLICITLY STATED IN THE CORRESPONDING FILE.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of version 2 of the GNU General Public License
+ * and of version 3 of the GNU Affero General Public License along with the Geneva
+ * library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard headers go here
+
+// Boost headers go here
+
+#include <boost/version.hpp>
+
+#if BOOST_VERSION < 103600
+#error "Error: Boost should at least have version 1.36 !"
+#endif /* BOOST_VERSION */
+
+#ifndef GCHARPARAMETER_HPP_
+#define GCHARPARAMETER_HPP_
+
+// GenEvA headers go here
+
+#include "GNumericParameterT.hpp"
+
+namespace Gem {
+namespace Util {
+
+/*************************************************************************/
+/**
+ * A parameter type used for the communication with external programs. See
+ * GNumericParameterT.hpp for further details.
+ */
+typedef GNumericParameterT<char> GCharParameter;
+
+} /* namespace Util */
+} /* namespace Gem */
+
+#endif /* GCHARPARAMETER_HPP_ */
---
+++ src/GDoubleParameter.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file GDoubleParameter.cpp
+ */
+
+/* Copyright (C) 2009 Dr. Ruediger Berlich
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License,
+ * or, at your option, under the terms of version 2 of the GNU General Public
+ * License, as published by the Free Software Foundation.
+ *
+ * NOTE THAT THIS FORM OF DUAL-LICENSING DOES NOT APPLY TO ANY OTHER FILES
+ * OF THE GENEVA LIBRARY, UNLESS THIS IS EXPLICITLY STATED IN THE CORRESPONDING FILE.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of version 2 of the GNU General Public License
+ * and of version 3 of the GNU Affero General Public License along with the Geneva
+ * library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "GDoubleParameter.hpp"
+
+// Included here so no conflicts occur. See explanation at
+// http://www.boost.org/libs/serialization/doc/special.html#derivedpointers
+#include <boost/serialization/export.hpp>
+BOOST_CLASS_EXPORT(Gem::Util::GDoubleParameter)
