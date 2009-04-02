6ee4184e70b6c8c1c9637095f142f81364e4773b
rberlich <Unknown>
2009-04-02 18:44:49 +0000

    Towards comprehensive unit tests for the entire Geneva library

---
+++ tests/UnitTests/GCharFlipAdaptor/GCharFlipAdaptor_test.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file GCharFlipAdaptor_test.cpp
+ *
+ * This test checks all public member functions of the GBooleanAdaptor adaptor
+ * class. In addition, it attempts to check parent classes, in particular the
+ * GObject class. You should run this test both in DEBUG and in RELEASE mode,
+ * as some functions may work differently in this case.
+ */
+
+/* Copyright (C) 2009 Dr. Ruediger Berlich
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard header files go here
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// Boost header files go here
+
+#define BOOST_TEST_MODULE GCharFlipAdaptor_test
+#include <boost/test/unit_test.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+// Geneva header files go here
+#include "GObject.hpp"
+#include "GCharFlipAdaptor.hpp"
+#include "GenevaExceptions.hpp"
+#include "GLogger.hpp"
+#include "GLogTargets.hpp"
+#include "GRandom.hpp"
+
+using namespace Gem;
+using namespace Gem::GenEvA;
+using namespace Gem::GLogFramework;
+
+/***********************************************************************************/
+// This test checks as much as possible of the functionality
+// provided by the GCharFlipAdaptor class.
+BOOST_AUTO_TEST_CASE( gcharflipadaptor_test_no_failure_expected )
+{
+	GRandom gr;
+
+}
+
+/***********************************************************************************/
+
+// EOF
