6a21fe01efdcb71daaff09b5bdffb985cb9d3f9e
rberlich <Unknown>
2009-04-02 21:50:29 +0000

    Towards a comprehensive test suite for the Geneva library

---
+++ tests/UnitTests/GBasePopulation_test/GBasePopulation_test.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file GBasePopulation_test.cpp
+ *
+ * This test checks most public member functions of the GBasePopulation class.
+ */
+
+/* Copyright (C) 2009 Dr. Ruediger Berlich
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard header files go here
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// Boost header files go here
+
+#define BOOST_TEST_MODULE GBoolean_test
+#include <boost/test/unit_test.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+// Geneva header files go here
+#include "GenevaExceptions.hpp"
+#include "GLogger.hpp"
+#include "GLogTargets.hpp"
+#include "GRandom.hpp"
+#include "GBasePopulation.hpp"
+
+using namespace Gem;
+using namespace Gem::Util;
+using namespace Gem::GenEvA;
+using namespace Gem::GLogFramework;
+
+/***********************************************************************************/
+// This test checks as much as possible of the functionality
+// provided by the GObject class.
+BOOST_AUTO_TEST_CASE( gbasepopulation_test_no_failure_expected )
+{
+	GRandom gr;
+
+}
+
+/***********************************************************************************/
+
+// EOF
