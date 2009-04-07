581c3aff89fa0cc3446995497abb107314672872
rberlich <Unknown>
2009-04-07 18:30:41 +0000

    Wrote some test code for GBooleanCollection. Not yet finished.

--- tests/UnitTests/GBooleanCollection_test.cpp
+++ tests/UnitTests/GBooleanCollection_test.cpp
@@ -37,6 +37,7 @@
 #include "GLogTargets.hpp"
 #include "GRandom.hpp"
 #include "GBooleanCollection.hpp"
+#include "GBooleanAdaptor.hpp"
 
 using namespace Gem;
 using namespace Gem::Util;
@@ -45,8 +46,9 @@ using namespace Gem::GLogFramework;
 
 /***********************************************************************************/
 // This test suite checks as much as possible of the functionality provided
-// by the GBooleanCollection class.
-BOOST_AUTO_TEST_SUITE(GBooleanCollection)
+// by the GBooleanCollection class, as well as some important parent classes
+// (GParameterCollectionT and GStdSimpleVectorInterface).
+BOOST_AUTO_TEST_SUITE(GBooleanCollectionSuite)
 
 /***********************************************************************************/
 // Test features that are expected to work
@@ -54,6 +56,109 @@ BOOST_AUTO_TEST_CASE(GBooleanCollection_no_failure_expected )
 {
 	GRandom gr;
 
+	// Construction in different modes
+	GBooleanCollection gbc0; // default construction, should be empty
+	BOOST_CHECK(gbc0.empty());
+
+	GBooleanCollection gbc1(100);  // 100 items
+	GBooleanCollection gbc1_2(100);  // 100 items
+	BOOST_CHECK(gbc1.size() == 100);
+	BOOST_CHECK(gbc1_2.size() == 100);
+	BOOST_CHECK(gbc1 != gbc1_2);
+
+	GBooleanCollection gbc2(100,0.7);  // 100 items, "true" is preferred
+	GBooleanCollection gbc2_2(100,0.7);  // 100 items, "true" is preferred
+	BOOST_CHECK(gbc2.size() == 100);
+	BOOST_CHECK(gbc2_2.size() == 100);
+	BOOST_CHECK(gbc2 != gbc2_2);
+
+	// Copy construction
+	GBooleanCollection gbc3(gbc2);
+	BOOST_CHECK(gbc3 == gbc2);
+
+	// Assignment
+	GBooleanCollection gbc4;
+	gbc4 = gbc3;
+	BOOST_CHECK(gbc4 == gbc2);
+
+	// Cloning and loading
+	GObject *gbc5 = gbc4.clone();
+	GBooleanCollection gbc6;
+	gbc6.load(gbc5);
+	delete gbc5;
+	BOOST_CHECK(gbc6 == gbc2);
+
+	// Adding random data using two different methods
+	gbc6.addRandomData(100);
+	BOOST_CHECK(gbc6 != gbc2);
+	BOOST_CHECK(gbc6.size() == 200);
+	gbc6.addRandomData(1800, 0.1);
+	BOOST_CHECK(gbc6.size() == 2000);
+
+	// Adding an adaptor
+	boost::shared_ptr<GBooleanAdaptor> gba(new GBooleanAdaptor());
+	gbc6.addAdaptor(gba);
+
+	const std::size_t NMUTATIONS=1000;
+	GBooleanCollection gbc6_old(gbc6);
+	for(std::size_t i=0; i<NMUTATIONS; i++) {
+		gbc6.mutate();
+		BOOST_CHECK(gbc6 != gbc6_old);
+		gbc6_old = gbc6;
+	}
+
+	// Test of serialization in different modes
+	// Test serialization and loading in different serialization modes
+	{ // plain text format
+		// Copy construction of a new object
+		GBooleanCollection gbc7(100);
+		GBooleanCollection gbc7_cp(gbc7);
+
+		// Check equalities and inequalities
+		BOOST_CHECK(gbc7_cp == gbc7);
+		// Re-assign a new value to gbc7_cp
+		gbc7_cp.addRandomData(100);
+		BOOST_CHECK(gbc7_cp.size() == 200);
+		BOOST_CHECK(gbc7_cp != gbc7);
+
+		// Serialize gbc7 and load into gbc7_co, check equalities and similarities
+		BOOST_REQUIRE_NO_THROW(gbc7_cp.fromString(gbc7.toString(TEXTSERIALIZATION), TEXTSERIALIZATION));
+		BOOST_CHECK(gbc7_cp.isSimilarTo(gbc7, exp(-10)));
+	}
+
+	{ // XML format
+		// Copy construction of a new object
+		GBooleanCollection gbc7(100);
+		GBooleanCollection gbc7_cp(gbc7);
+
+		// Check equalities and inequalities
+		BOOST_CHECK(gbc7_cp == gbc7);
+		// Re-assign a new value to gbc7_cp
+		gbc7_cp.addRandomData(100);
+		BOOST_CHECK(gbc7_cp.size() == 200);
+		BOOST_CHECK(gbc7_cp != gbc7);
+
+		// Serialize gbc7 and load into gbc7_co, check equalities and similarities
+		BOOST_REQUIRE_NO_THROW(gbc7_cp.fromString(gbc7.toString(XMLSERIALIZATION), XMLSERIALIZATION));
+		BOOST_CHECK(gbc7_cp.isSimilarTo(gbc7, exp(-10)));
+	}
+
+	{ // binary test format
+		// Copy construction of a new object
+		GBooleanCollection gbc7(100);
+		GBooleanCollection gbc7_cp(gbc7);
+
+		// Check equalities and inequalities
+		BOOST_CHECK(gbc7_cp == gbc7);
+		// Re-assign a new value to gbc7_cp
+		gbc7_cp.addRandomData(100);
+		BOOST_CHECK(gbc7_cp.size() == 200);
+		BOOST_CHECK(gbc7_cp != gbc7);
+
+		// Serialize gbc7 and load into gbc7_co, check equalities and similarities
+		BOOST_REQUIRE_NO_THROW(gbc7_cp.fromString(gbc7.toString(BINARYSERIALIZATION), BINARYSERIALIZATION));
+		BOOST_CHECK(gbc7_cp.isEqualTo(gbc7));
+	}
 }
 
 /***********************************************************************************/
@@ -62,6 +167,9 @@ BOOST_AUTO_TEST_CASE( GBooleanCollection_failures_expected )
 {
 	GRandom gr;
 
+	// Self-assignment should throw
+	GBooleanCollection gbc(100);
+	BOOST_CHECK_THROW(gbc.load(&gbc), Gem::GenEvA::geneva_error_condition);
 }
 /***********************************************************************************/
 
