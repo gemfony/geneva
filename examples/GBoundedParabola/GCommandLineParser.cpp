ed5e7237560231a9adfe2d6eb7ad665affc636de
rberlich <Unknown>
2009-04-07 12:17:16 +0000

    Added a new example which demonstrates the usage of the GBoundedDouble collection.

---
+++ examples/GBoundedParabola/GCommandLineParser.cpp
@@ -0,0 +1,152 @@
+/**
+ * @file GCommandLineParser.cpp
+ */
+
+/* Copyright (C) 2004-2008 Dr. Ruediger Berlich
+ * Copyright (C) 2008 Forschungszentrum Karlsruhe GmbH
+ *
+ * This file is part of the Geneva library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "GCommandLineParser.hpp"
+
+namespace Gem
+{
+namespace GenEvA
+{
+
+/************************************************************************************************/
+/**
+ * A function that parses the command line for all required parameters
+ */
+bool parseCommandLine(int argc, char **argv,
+					  std::size_t& parabolaDimension,
+					  double& parabolaMin,
+					  double& parabolaMax,
+					  boost::uint32_t& adaptionThreshold,
+					  boost::uint16_t& nProducerThreads,
+					  std::size_t& populationSize,
+					  std::size_t& nParents,
+					  boost::uint32_t& maxGenerations,
+					  long& maxMinutes,
+					  boost::uint32_t& reportGeneration,
+					  recoScheme& rScheme,
+					  bool& parallel,
+					  bool& verbose)
+{
+	boost::uint16_t recombinationScheme=0;
+
+	try{
+		// Check the command line options. Uses the Boost program options library.
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help,h", "emit help message")
+			("parabolaDimension,d", po::value<std::size_t>(&parabolaDimension)->default_value(DEFAULTPARABOLADIMENSION),
+					"number of dimensions in the parabola")
+			("parabolaMin,m", po::value<double>(&parabolaMin)->default_value(DEFAULTPARABOLAMIN),
+					"Lower boundary for random numbers")
+			("adaptionThreshold,a", po::value<boost::uint32_t>(&adaptionThreshold)->default_value(DEFAULTADAPTIONTHRESHOLD),
+					"Number of calls to mutate after which mutation parameters should be adapted")
+			("parabolaMax,M", po::value<double>(&parabolaMax)->default_value(DEFAULTPARABOLAMAX),
+					"Upper boundary for random numbers")
+			("nProducerThreads,n",po::value<boost::uint16_t>(&nProducerThreads)->default_value(DEFAULTNPRODUCERTHREADS),
+					"The amount of random number producer threads")
+			("populationSize,S",po::value<std::size_t>(&populationSize)->default_value(DEFAULTPOPULATIONSIZE),
+					"The size of the super-population")
+			("nParents,P",po::value<std::size_t>(&nParents)->default_value(DEFAULTNPARENTS),
+					"The number of parents in the population") // Needs to be treated separately
+			("maxGenerations,G", po::value<boost::uint32_t>(&maxGenerations)->default_value(DEFAULTMAXGENERATIONS),
+					"maximum number of generations in the population")
+			("maxMinutes,X", po::value<long>(&maxMinutes)->default_value(DEFAULTMAXMINUTES),
+					"The maximum number of minutes the optimization of the population should run")
+			("reportGeneration,R",po::value<boost::uint32_t>(&reportGeneration)->default_value(DEFAULTREPORTGENERATION),
+					"The number of generations after which information should be emitted in the super-population")
+			("rScheme,E",po::value<boost::uint16_t>(&recombinationScheme)->default_value(DEFAULTRSCHEME),
+					"The recombination scheme for the super-population")
+			("parralel,p", po::value<bool>(&parallel)->default_value(DEFAULTPARALLEL),
+			                "Whether or not to run this optimization in multi-threaded mode")
+			("verbose,v",po::value<bool>(&verbose)->default_value(DEFAULTVERBOSE),
+					"Whether additional information should be emitted")
+		;
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		// Emit a help message, if necessary
+		if (vm.count("help")) {
+			 std::cout << desc << std::endl;
+			 return false;
+		}
+
+		// Check the number of parents in the super-population
+		if(2*nParents > populationSize){
+			std::cout << "Error: Invalid number of parents inpopulation" << std::endl
+				      << "nParents       = " << nParents << std::endl
+				      << "populationSize = " << populationSize << std::endl;
+
+			return false;
+		}
+
+		// Check the parabolaMin/Max parameters
+		if(parabolaMin >= parabolaMax){
+			std::cout << "Error: Invalid parabolaMin/Max parameters" << std::endl
+				      << "parabolaMin = " << parabolaMin << std::endl
+				      << "parabolaMax = " << parabolaMax << std::endl;
+
+			return false;;
+		}
+
+		// Workaround for assigment problem with rScheme
+		if(recombinationScheme==(boost::uint16_t)VALUERECOMBINE)
+			rScheme=VALUERECOMBINE;
+		else if(recombinationScheme==(boost::uint16_t)RANDOMRECOMBINE)
+			rScheme=RANDOMRECOMBINE;
+		else if(recombinationScheme==(boost::uint16_t)DEFAULTRECOMBINE)
+			rScheme=DEFAULTRECOMBINE;
+		else {
+			std::cout << "Error: Invalid recombination scheme in population: " << recombinationScheme << std::endl;
+			return false;
+		}
+
+		if(verbose){
+			std::cout << std::endl
+				      << "Running with the following options:" << std::endl
+					  << "parabolaDimension = " << parabolaDimension << std::endl
+					  << "parabolaMin = " << parabolaMin << std::endl
+					  << "parabolaMax = " << parabolaMax << std::endl
+					  << "adaptionThreshold = " << adaptionThreshold << std::endl
+					  << "nProducerThreads = " << (boost::uint16_t)nProducerThreads << std::endl // boost::uint8_t not printable on gcc ???
+					  << "populationSize = " << populationSize << std::endl
+					  << "nParents = " << nParents << std::endl
+					  << "maxGenerations = " << maxGenerations << std::endl
+					  << "maxMinutes = " << maxMinutes << std::endl
+					  << "reportGeneration = " << reportGeneration << std::endl
+					  << "rScheme = " << (boost::uint16_t)rScheme << std::endl
+					  << std::endl;
+		}
+	}
+	catch(...){
+		std::cout << "Error parsing the command line" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+/************************************************************************************************/
+
+} /* namespace GenEvA */
+} /* namespace Gem */
---
+++ examples/SampleIndividuals/GBoundedParabolaIndividual.hpp
@@ -0,0 +1,242 @@
+/**
+ * @file BoundedGParabolaIndividual.hpp
+ */
+
+/* Copyright (C) 2009 Dr. Ruediger Berlich
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard header files go here
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include <vector>
+
+// Boost header files go here
+#include <boost/shared_ptr.hpp>
+
+#ifndef GPARABOLAINDIVIDUAL_HPP_
+#define GPARABOLAINDIVIDUAL_HPP_
+
+// GenEvA header files go here
+#include "GParameterSet.hpp"
+#include "GDoubleGaussAdaptor.hpp"
+#include "GBoundedDoubleCollection.hpp"
+#include "GBoundedDouble.hpp"
+
+namespace Gem
+{
+namespace GenEvA
+{
+
+/************************************************************************************************/
+/**
+ * This individual searches for the minimum of a simple parabola in n dimensions. It is meant
+ * as an example of how to set up custom individuals. In contrast the the GBoundedParabolaIndividual,
+ * this class stores collections of GBoundedDouble objects.
+ */
+class GBoundedParabolaIndividual
+	:public GParameterSet
+{
+	///////////////////////////////////////////////////////////////////////
+	friend class boost::serialization::access;
+
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version) {
+		using boost::serialization::make_nvp;
+
+		ar & make_nvp("ParameterSet",
+				boost::serialization::base_object<GParameterSet>(*this));
+		// add all local variables here, if you want them to be serialized. E.g.:
+		// ar & make_nvp("myLocalVar_",myLocalVar_);
+		// This also works with objects, if they have a corresponding serialize() function.
+	}
+	///////////////////////////////////////////////////////////////////////
+
+public:
+	/********************************************************************************************/
+	/**
+	 * The default constructor.
+	 */
+	GBoundedParabolaIndividual()
+	{ /* nothing */ }
+
+	/********************************************************************************************/
+	/**
+	 * A constructor which initializes the individual with a suitable set of random double values.
+	 *
+	 * @param sz The desired size of the double collection
+	 * @param min The minimum value of the random numbers to fill the collection
+	 * @param max The maximum value of the random numbers to fill the collection
+	 * @param as The number of calls to GDoubleGaussAdaptor::mutate after which mutation should be adapted
+	 */
+	GBoundedParabolaIndividual(const std::size_t& sz, const double& min, const double& max, const boost::uint32_t& as){
+		// Set up a GBoundedDoubleCollection with sz values, each initialized
+		// with a random number in the range [min,max[
+		boost::shared_ptr<GBoundedDoubleCollection> gbdc(new GBoundedDoubleCollection());
+
+		// Set up and register GBoundedDouble objects in the desired value range
+		for(std::size_t i=0; i<sz; i++) {
+			// GBoundedDouble will iteself assign a random value in the desired value range
+			boost::shared_ptr<GBoundedDouble> gbd_ptr(new GBoundedDouble(min, max));
+
+			// Set up and register an adaptor for the object, so it knows how to be mutated.
+			// We want a sigma of 1, a sigma-adaption of 0.001, a minimum sigma of 0.000001
+			// and a maximum sigma of 5.
+			boost::shared_ptr<GDoubleGaussAdaptor> gdga(new GDoubleGaussAdaptor(1.,0.001,0.000001,5));
+			gdga->setAdaptionThreshold(as);
+
+			gbd_ptr->addAdaptor(gdga);
+
+			// Make the GBoundedDouble known to the collection
+			gbdc->push_back(gbd_ptr);
+		}
+
+		// Make the GBoundedDouble collection known to this individual
+		this->data.push_back(gbdc);
+	}
+
+	/********************************************************************************************/
+	/**
+	 * A standard copy constructor
+	 */
+	GBoundedParabolaIndividual(const GBoundedParabolaIndividual& cp)
+		:GParameterSet(cp)
+	{ /* nothing */	}
+
+	/********************************************************************************************/
+	/**
+	 * The standard destructor
+	 */
+	virtual ~GBoundedParabolaIndividual()
+	{ /* nothing */	}
+
+	/********************************************************************************************/
+	/**
+	 * A standard assignment operator
+	 */
+	const GBoundedParabolaIndividual& operator=(const GBoundedParabolaIndividual& cp){
+		GBoundedParabolaIndividual::load(&cp);
+		return *this;
+	}
+
+	/********************************************************************************************/
+	/**
+	 * Creates a deep clone of this object
+	 *
+	 * @return A deep clone of this object, camouflaged as a GObject
+	 */
+	virtual GObject* clone() const {
+		return new GBoundedParabolaIndividual(*this);
+	}
+
+	/********************************************************************************************/
+	/**
+	 * Loads the data of another GBoundedParabolaIndividual, camouflaged as a GObject
+	 *
+	 * @param cp A copy of another GBoundedParabolaIndividual, camouflaged as a GObject
+	 */
+	virtual void load(const GObject* cp) {
+		// We have no local data. Hence we can just pass the pointer to our parent class.
+		// Note that we'd have to use the GObject::conversion_cast() function otherwise.
+		GParameterSet::load(cp);
+	}
+
+	/*******************************************************************************************/
+	/**
+	 * Checks for equality with another GBoundedParabolaIndividual object
+	 *
+	 * @param  cp A constant reference to another GBoundedParabolaIndividual object
+	 * @return A boolean indicating whether both objects are equal
+	 */
+	bool operator==(const GBoundedParabolaIndividual& cp) const {
+		return GBoundedParabolaIndividual::isEqualTo(cp);
+	}
+
+	/*******************************************************************************************/
+	/**
+	 * Checks for inequality with another GBoundedParabolaIndividual object
+	 *
+	 * @param  cp A constant reference to another GBoundedParabolaIndividual object
+	 * @return A boolean indicating whether both objects are inequal
+	 */
+	bool operator!=(const GBoundedParabolaIndividual& cp) const {
+		return !GBoundedParabolaIndividual::isEqualTo(cp);
+	}
+
+	/*******************************************************************************************/
+	/**
+	 * Checks for equality with another GBoundedParabolaIndividual object.  If T is an object type,
+	 * then it must implement operator!= .
+	 *
+	 * @param  cp A constant reference to another GBoundedParabolaIndividual object
+	 * @return A boolean indicating whether both objects are equal
+	 */
+	virtual bool isEqualTo(const GBoundedParabolaIndividual& cp) const {
+		// Check equality of the parent class
+		if(!GParameterSet::isEqualTo(cp)) return false;
+
+		return true;
+	}
+
+	/*******************************************************************************************/
+	/**
+	 * Checks for similarity with another GBoundedParabolaIndividual object.  As we do not know the
+	 * type of T, we need to create a specialization of this function for typeof(T)==double
+	 *
+	 * @param  cp A constant reference to another GBoundedParabolaIndividual object
+	 * @param limit A double value specifying the acceptable level of differences of floating point values
+	 * @return A boolean indicating whether both objects are similar to each other
+	 */
+	virtual bool isSimilarTo(const GBoundedParabolaIndividual& cp, const double& limit=0) const {
+		// Check equality of the parent class
+		if(!GParameterSet::isSimilarTo(cp, limit)) return false;
+
+		return true;
+	}
+
+protected:
+	/********************************************************************************************/
+	/**
+	 * The actual fitness calculation takes place here
+	 *
+	 * @return The value of this object
+	 */
+	virtual double fitnessCalculation(){
+		// Compile in DEBUG mode in order to check this conversion
+		// The desired collection is in the first position, hence we pass a 0 to the function.
+		boost::shared_ptr<GBoundedDoubleCollection> gbdc_load = parameterbase_cast<GBoundedDoubleCollection>(0);
+
+		// Great - now we can do the actual calculations. We do this the fancy way ...
+		// Note that (*it) will be of type boost::shared_ptr<GBoundedDouble> .
+		double result = 0;
+		GBoundedDoubleCollection::iterator it;
+		for(it=gbdc_load->begin(); it!=gbdc_load->end(); ++it)
+			result += std::pow((*it)->value(), 2);
+
+		return result;
+	}
+
+	/********************************************************************************************/
+};
+
+} /* namespace GenEvA */
+} /* namespace Gem */
+
+#include <boost/serialization/export.hpp>
+BOOST_CLASS_EXPORT(Gem::GenEvA::GBoundedParabolaIndividual)
+
+#endif /* GPARABOLAINDIVIDUAL_HPP_ */
