97df310067b2d8b2b67b0e2f0090dca211480909
rberlich <Unknown>
2009-08-20 09:26:02 +0000

    En route to a global seed manager

---
+++ include/GSeedManager.hpp
@@ -0,0 +1,83 @@
+/**
+ * @file GSeedManager.hpp
+ */
+
+/* Copyright (C) 2009 Dr. Ruediger Berlich
+ *
+ * This file is part of Geneva, Gemfony scientific's optimization library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Standard headers go here
+
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+
+// Includes check for correct Boost version(s)
+#include "GGlobalDefines.hpp"
+
+// Boost headers go here
+
+#include <boost/cstdint.hpp>
+#include <boost/random.hpp>
+#include <boost/date_time.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/shared_array.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/exception.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/limits.hpp>
+#include <boost/filesystem.hpp>
+
+/**
+ * Check that we have support for threads. This collection of classes is useless
+ * without this.
+ */
+#ifndef BOOST_HAS_THREADS
+#error "Error: No support for Boost.threads available."
+#endif
+
+#ifndef GSEEDMANAGER_HPP_
+#define GSEEDMANAGER_HPP_
+
+// For Microsoft-compatible compilers
+#if defined(_MSC_VER)  &&  (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+
+// GenEvA headers go here
+#include "GBoundedBufferT.hpp"
+#include "GEnums.hpp"
+#include "GenevaExceptions.hpp"
+
+/****************************************************************************/
+
+namespace Gem {
+namespace Util {
+
+} /* namespace Util */
+} /* namespace Gem */
+
+#endif /* GSEEDMANAGER_HPP_ */
