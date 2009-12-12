d526d5f6fb8234347cfc6d8854198f66b3be935c
rberlich <Unknown>
2009-12-12 21:22:09 +0100

    Work towards the addition of "personality traits" for individuals. The idea is to hide specific traits of individuals which are particular to a given optimization algorithm in a separate object, which can be exchanged dependending on the chosen algorithm. This is in preparation of the implementation of swarm algorithms and gradient descents.

---
+++ include/GPersonalityTraits.hpp
@@ -0,0 +1,141 @@
+/**
+ * @file GPersonalityTraits.hpp
+ */
+
+/* Copyright (C) Dr. Ruediger Berlich and Karlsruhe Institute of Technology
+ * (University of the State of Baden-Wuerttemberg and National Laboratory
+ * of the Helmholtz Association)
+ *
+ * Contact: info [at] gemfony (dot) com
+ *
+ * This file is part of the Geneva library, Gemfony scientific's optimization
+ * library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For further information on Gemfony scientific and Geneva, visit
+ * http://www.gemfony.com .
+ */
+
+
+// Standard headers go here
+#include <string>
+
+// Includes check for correct Boost version(s)
+#include "GGlobalDefines.hpp"
+
+// Boost headers go here
+#include <boost/cstdint.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/tracking.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <boost/serialization/export.hpp>
+
+
+#ifndef GPERSONALITYTRAITS_HPP_
+#define GPERSONALITYTRAITS_HPP_
+
+// For Microsoft-compatible compilers
+#if defined(_MSC_VER)  &&  (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+
+// GenEvA headers go here
+#include "GObject.hpp"
+
+namespace Gem {
+namespace GenEvA {
+
+/*********************************************************************************/
+/**
+ * This is the base class for a small hierarchy that encapsulates information
+ * relevant to particular optimization algorithms. The information is stored in
+ * individuals (i.e. the parameter sets which are subject to a given optimization
+ * problem). In this sense, individuals can take on more than one role or
+ * personality. Note that this class is purely virtual. It can only be used in
+ * conjunction with a derived personality.
+ */
+class GPersonalityTraits :public GObject
+{
+	///////////////////////////////////////////////////////////////////////
+	friend class boost::serialization::access;
+
+	template<typename Archive>
+	void serialize(Archive & ar, const unsigned int){
+	  using boost::serialization::make_nvp;
+	  ar & make_nvp("GObject", boost::serialization::base_object<GObject>(*this));
+	  ar & make_nvp("parentAlgIteration_", parentAlgIteration_);
+	  ar & make_nvp("command_", command_);
+	}
+	///////////////////////////////////////////////////////////////////////
+
+public:
+	/** @brief The default constructor */
+	GPersonalityTraits();
+	/** @brief The copy contructor */
+	GPersonalityTraits(const GPersonalityTraits&);
+	/** @brief The standard destructor */
+	virtual ~GPersonalityTraits();
+
+	/** @brief Checks for equality with another GPersonalityTraits object */
+	bool operator==(const GPersonalityTraits&) const;
+	/** @brief Checks for inequality with another GPersonalityTraits object */
+	bool operator!=(const GPersonalityTraits&) const;
+	/** @brief Checks for equality with another GPersonalityTraits object */
+	virtual bool isEqualTo(const GObject&, const boost::logic::tribool& expected = boost::logic::indeterminate) const;
+	/** @brief Checks for similarity with another GPersonalityTraits object */
+	virtual bool isSimilarTo(const GObject&, const double& limit, const boost::logic::tribool& expected = boost::logic::indeterminate) const;
+
+	/** @brief Creates a deep clone of this object */
+	virtual GObject* clone() const = 0;
+	/** @brief Loads the data of another GPersonalityTraits object */
+	virtual void load(const GObject*);
+
+	/** @brief Allows to set the current iteration of the parent optimization algorithm. */
+	void setParentAlgIteration(const boost::uint32_t&);
+	/** @brief Gives access to the parent optimization algorithm's iteration */
+	boost::uint32_t getParentAlgIteration() const;
+	/** @brief Sets a command to be performed by a remote client. */
+	void setCommand(const std::string&);
+	/** @brief Retrieves the command to be performed by a remote client. */
+	std::string getCommand() const;
+
+private:
+	boost::uint32_t parentAlgIteration_; ///< The iteration of the parent algorithm's optimization cycle
+	std::string command_;
+};
+
+/*********************************************************************************/
+
+} /* namespace GenEvA */
+} /* namespace Gem */
+
+/*********************************************************************************/
+/**
+ * @brief Needed for Boost.Serialization
+ */
+BOOST_SERIALIZATION_ASSUME_ABSTRACT(Gem::GenEvA::GPersonalityTraits)
+/*********************************************************************************/
+
+#endif /* GPERSONALITYTRAITS_HPP_ */
---
+++ src/GPersonalityTraits.cpp
@@ -0,0 +1,193 @@
+/**
+ * @file GPersonalityTraits.cpp
+ */
+
+/* Copyright (C) Dr. Ruediger Berlich and Karlsruhe Institute of Technology
+ * (University of the State of Baden-Wuerttemberg and National Laboratory
+ * of the Helmholtz Association)
+ *
+ * Contact: info [at] gemfony (dot) com
+ *
+ * This file is part of the Geneva library, Gemfony scientific's optimization
+ * library.
+ *
+ * Geneva is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For further information on Gemfony scientific and Geneva, visit
+ * http://www.gemfony.com .
+ */
+
+#include "GPersonalityTraits.hpp"
+
+// Included here so no conflicts occur. See explanation at
+// http://www.boost.org/libs/serialization/doc/special.html#derivedpointers
+#include <boost/serialization/export.hpp>
+BOOST_CLASS_EXPORT(Gem::GenEvA::GPersonalityTraits)
+
+namespace Gem {
+namespace GenEvA {
+
+/*****************************************************************************/
+/**
+ * The default constructor
+ */
+GPersonalityTraits::GPersonalityTraits():
+	parentAlgIteration_(0),
+	command_("")
+{ /* nothing */ }
+
+/*****************************************************************************/
+/**
+ * The copy constructor
+ */
+GPersonalityTraits::GPersonalityTraits(const GPersonalityTraits& cp):
+	parentAlgIteration_(cp.parentAlgIteration_),
+	command_(cp.command_)
+{ /* nothing */ }
+
+/*****************************************************************************/
+/**
+ * The standard destructor. No local, dynamically allocated data,
+ * hence it does nothing.
+ */
+GPersonalityTraits::~GPersonalityTraits()
+{ /* nothing */ }
+
+/*****************************************************************************/
+/**
+ * Checks for equality with another GPersonalityTraits object
+ *
+ * @param  cp A constant reference to another GPersonalityTraits object
+ * @return A boolean indicating whether both objects are equal
+ */
+bool GPersonalityTraits::operator==(const GPersonalityTraits& cp) const {
+	return GPersonalityTraits::isEqualTo(cp, boost::logic::indeterminate);
+}
+
+/*****************************************************************************/
+/**
+ * Checks for inequality with another GPersonalityTraits object
+ *
+ * @param  cp A constant reference to another GPersonalityTraits object
+ * @return A boolean indicating whether both objects are inequal
+ */
+bool GPersonalityTraits::operator!=(const GPersonalityTraits& cp) const {
+	return !GPersonalityTraits::isEqualTo(cp, boost::logic::indeterminate);
+}
+
+/*****************************************************************************/
+/**
+ * Checks for equality with another GPersonalityTraits object.
+ *
+ * @param  cp A constant reference to another GPersonalityTraits object
+ * @return A boolean indicating whether both objects are equal
+ */
+bool GPersonalityTraits::isEqualTo(const GObject& cp, const boost::logic::tribool& expected) const {
+    using namespace Gem::Util;
+
+	// Check that we are indeed dealing with a GPersonalityTraits reference
+	const GPersonalityTraits *gpt_load = GObject::conversion_cast(&cp,  this);
+
+	// Check for equality of our parent class
+	if(!GObject::isEqualTo(*gpt_load, expected)) return  false;
+
+	// Then we take care of the local data
+	if(checkForInequality("GPersonalityTraits", parentAlgIteration_, gpt_load->parentAlgIteration_,"parentAlgIteration_", "gpt_load->parentAlgIteration_", expected)) return false;
+	if(checkForInequality("GPersonalityTraits", command_, gpt_load->command_,"currentFitness_", "gpt_load->currentFitness_", expected)) return false;
+
+	return true;
+}
+
+/*****************************************************************************/
+/**
+ * Checks for similarity with another GPersonalityTraits object.
+ *
+ * @param  cp A constant reference to another GPersonalityTraits object
+ * @param limit A double value specifying the acceptable level of differences of floating point values
+ * @return A boolean indicating whether both objects are similar to each other
+ */
+bool GPersonalityTraits::isSimilarTo(const GObject& cp, const double& limit, const boost::logic::tribool& expected) const {
+    using namespace Gem::Util;
+
+	// Check that we are indeed dealing with a GPersonalityTraits reference
+	const GPersonalityTraits *gpt_load = GObject::conversion_cast(&cp,  this);
+
+	// Check for equality of our parent class
+	if(!GObject::isSimilarTo(*gpt_load, limit, expected)) return false;
+
+	// Then we take care of the local data
+	if(checkForDissimilarity("GPersonalityTraits", parentAlgIteration_, gpt_load->parentAlgIteration_, limit, "parentAlgIteration_", "gpt_load->parentAlgIteration_", expected)) return false;
+	if(checkForDissimilarity("GPersonalityTraits", command_, gpt_load->command_, limit, "command_", "gpt_load->command_", expected)) return false;
+
+	return true;
+}
+
+/*****************************************************************************/
+/**
+ * Loads the data of another GPersonalityTraits object
+ *
+ * @param cp A copy of another GPersonalityTraits object, camouflaged as a GObject
+ */
+void GPersonalityTraits::load(const GObject *cp) {
+	const GPersonalityTraits *gpt_load = this->conversion_cast(cp, this);
+
+	// Load the parent class'es data
+	GObject::load(cp);
+
+	// Then load our local data
+	parentAlgIteration_ = gpt_load->parentAlgIteration_;
+	command_ = gpt_load->command_;
+}
+
+/*****************************************************************************/
+/**
+ * Allows to set the current iteration of the parent optimization algorithm.
+ *
+ * @param parentAlgIteration The current iteration of the optimization algorithm
+ */
+void GPersonalityTraits::setParentAlgIteration(const boost::uint32_t& parentAlgIteration) {
+	parentAlgIteration_ = parentAlgIteration;
+}
+
+/*****************************************************************************/
+/**
+ * Gives access to the parent optimization algorithm's iteration
+ *
+ * @return The parent optimization algorithm's current iteration
+ */
+boost::uint32_t GPersonalityTraits::getParentAlgIteration() const {
+	return parentAlgIteration_;
+}
+
+/*****************************************************************************/
+/**
+ * Sets a command to be performed by a remote client.
+ *
+ * @param command The command to be performed by a remote client
+ */
+void GPersonalityTraits::setCommand(const std::string& command) {
+	command_ = command;
+}
+
+/*****************************************************************************/
+/**
+ * Retrieves the command to be performed by a remote client.
+ *
+ * @return The command to be performed by a remote client.
+ */
+std::string GPersonalityTraits::getCommand() const {
+	return command_;
+}
+
+} /* namespace GenEvA */
+} /* namespace Gem */
