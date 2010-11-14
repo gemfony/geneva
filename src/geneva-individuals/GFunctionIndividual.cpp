4cf61fc976daa59b9bb4b38fd50ead61584ac675
rberlich <Unknown>
2010-11-14 21:39:58 +0100

    Added GFunctionIndividual to new library

--- src/geneva-individuals/GFunctionIndividual.cpp
+++ src/geneva-individuals/GFunctionIndividual.cpp
@@ -29,8 +29,7 @@
  * http://www.gemfony.com .
  */
 
-#include "GFunctionIndividual.hpp"
-#include "GFunctionIndividualDefines.hpp"
+#include "geneva-individuals/GFunctionIndividual.hpp"
 
 #include <boost/serialization/export.hpp>
 BOOST_CLASS_EXPORT_GUID(Gem::Geneva::GFunctionIndividual<Gem::Geneva::PARABOLA>, "GFunctionIndividual_PARABOLA")
