39674c0974d19d5dbe4e1159790618b93ef872bf
rberlich <Unknown>
2010-11-18 14:29:04 +0100

    Fixed small inclusion problem for Boost beta check

--- include/geneva-individuals/GDelayIndividual.hpp
+++ include/geneva-individuals/GDelayIndividual.hpp
@@ -56,6 +56,8 @@
 
 // Geneva header files go here
 #include "geneva/GDoubleCollection.hpp"
+#include "geneva/GConstrainedDoubleObject.hpp"
+#include "geneva/GConstrainedDoubleObjectCollection.hpp"
 #include "geneva/GParameterSet.hpp"
 #include "geneva/GDoubleGaussAdaptor.hpp"
 #include "geneva-individuals/GIndividualFactoryT.hpp"
