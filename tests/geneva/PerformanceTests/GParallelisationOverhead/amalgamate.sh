0cb4b135ad6ca586c073462e361634786df651e5
rberlich <Unknown>
2011-04-21 01:51:54 +0200

    Further refined the amalgamation script

--- tests/geneva/PerformanceTests/GParallelisationOverhead/amalgamate.sh
+++ tests/geneva/PerformanceTests/GParallelisationOverhead/amalgamate.sh
@@ -30,8 +30,10 @@
 #
 ####################################################################
 # This script will take two "short" results of delay measurements
-# of equal length and produce a speedup graph. There are exactly
-# two arguments: the files holding the serial and the parallel measurements.
+# of equal length and produce a speedup graph. There are three
+# arguments: the files holding the serial and the parallel measurements,
+# and the number of parallel processing units --this determines the
+# maximum theoretical speedup.
 ####################################################################
 # !/bin/bash
 #
@@ -39,9 +41,10 @@
 # Set some variables
 RESULTFILE=speedup.C
 
-# Check that the number of command line options is at exactly two 
-# (the names of the files holding the serial and the parallel measurement).
-if [ $# -ne 2 ]; then
+# Check that the number of command line options is at exactly three 
+# (the names of the files holding the serial and the parallel measurement,
+# plus the number of parallel processing units
+if [ $# -ne 3 ]; then
 	echo "Usage: ./amalgamate.sh <serial result file> <parallel result file>"
 	exit
 fi
@@ -56,6 +59,19 @@ if [ ! -e $2 ]; then
     echo "Result file $2 with parallel results cannot be opened. Leaving"
     exit
 fi
+
+# Check that the number of parallel processing units is integral and > 0
+if [ ! $(echo "$3" | grep -E "^[0-9]+$") ]; then
+    echo "Error: $3 is not a valid integer. Leaving."
+    exit
+fi
+if [ ! $3 -gt 0 ];     then
+    echo "Error: $3 should at least be 1. Leaving"
+    exit
+fi
+
+# Assign the number of processing units
+nProcUnits=$3
 
 # Read in the serial timings
 mcnt=0
@@ -126,6 +142,7 @@ pcnt=0
 for p in "${parallel[@]}"; do
     echo "  parallel[$pcnt]=double($p);" >> ${RESULTFILE}
     echo "  speedup[$pcnt]=double(${serial[$pcnt]})/double(${parallel[$pcnt]});" >> ${RESULTFILE}
+    echo "  speedup[$pcnt] /= double(${nProcUnits});" >> ${RESULTFILE}
     let pcnt=$pcnt+1
 done
 
@@ -137,16 +154,23 @@ done
 
 let upperLimit=${nMeasurements}-1
 for spe in `seq 1 ${upperLimit}`; do
-	echo "  speedup_error[$spe] = sqrt(pow(${serial[$spe]}*${sdParallel[$spe]}/pow(${parallel[$spe]}, 2) + pow(${sdSerial[$spe]}/${parallel[$spe]}, 2));" >> ${RESULTFILE}
+	echo "  speedup_error[$spe] = sqrt(pow(${serial[$spe]}*${sdParallel[$spe]}/pow(${parallel[$spe]}, 2), 2) + pow(${sdSerial[$spe]}/${parallel[$spe]}, 2));" >> ${RESULTFILE}
+	echo "  speedup_error[$spe] /= double(${nProcUnits});" >> ${RESULTFILE}
 done
 
 # Emit the footer
 echo >> ${RESULTFILE}
-echo "  TGraphError *evGraph = new TGraphError($nMeasurements, delays, speedup, 0, speedup_error); " >> ${RESULTFILE}
+echo "  TGraphErrors *evGraph = new TGraphErrors($nMeasurements, delays, speedup, 0, speedup_error); " >> ${RESULTFILE}
 echo >> ${RESULTFILE}
 echo "  evGraph->SetMarkerStyle(2);" >> ${RESULTFILE}
 echo "  evGraph->SetMarkerSize(1.0);" >> ${RESULTFILE}
-echo "  evGraph->Draw(\"ACP\");" >> ${RESULTFILE}
+echo "  evGraph->Draw(\"AC\");" >> ${RESULTFILE}
+echo "  TLine *tl1 = new TLine(0.,1.,132, 1.);" >> ${RESULTFILE}
+echo "  tl1->SetLineStyle(2);" >> ${RESULTFILE}
+echo "  tl1->Draw();" >> ${RESULTFILE}
+echo "  TLatex *tt1 = new TLatex(20, 1.01, \"Speedup normalized to number of workers \#rightarrow upper limit is 1\");" >> ${RESULTFILE}
+echo "  tt1->SetTextSize(0.03);" >> ${RESULTFILE}
+echo "  tt1->Draw();" >> ${RESULTFILE}
 echo "  evGraph->GetXaxis()->SetTitle(\"Evaluation time/individual [s]\");" >> ${RESULTFILE}
-echo "  evGraph->GetYaxis()->SetTitle(\"Speedup\");" >> ${RESULTFILE}
+echo "  evGraph->GetYaxis()->SetTitle(\"Speedup for ${nProcUnits} parallel workers\");" >> ${RESULTFILE}
 echo "}" >> ${RESULTFILE}
