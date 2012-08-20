f1ce2165a77dfce75fcde39c0e01926d497a30f6
Ariel Garcia <garcia@kit.edu>
2012-08-20 10:40:44 +0200

    Remove another redundant stop condition that results in a warning

--- include/geneva/GStdPtrVectorInterfaceT.hpp
+++ include/geneva/GStdPtrVectorInterfaceT.hpp
@@ -193,7 +193,7 @@ public:
 			data.resize(cpSize);
 
 			// Then copy the elements
-			for(it=data.begin(), cp_it=cp.begin(); it!=data.end(), cp_it!=cp.end(); ++it, ++cp_it) {
+			for(it=data.begin(), cp_it=cp.begin(); it!=data.end(); ++it, ++cp_it) {
 				(*it)->GObject::load(*cp_it);
 			}
 		}
