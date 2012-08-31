76fc15c606da083deb6c68eafa909f0cd5dafb66
rberlich <Unknown>
2012-08-31 13:27:00 +0200

    Added two tests for adaptors

---
+++ tests/geneva/ManualTests/GDoubleBiGaussAdaptorTest/GDoubleBiGaussAdaptorTest.cpp
@@ -0,0 +1,182 @@
+/**
+ * @file GDoubleBiGaussAdaptorTest.cpp
+ */
+
+/*
+ * Copyright (C) Gemfony scientific UG (haftungsbeschraenkt)
+ *
+ * See the AUTHORS file in the top-level directory for a list of authors.
+ *
+ * Contact: contact [at] gemfony (dot) com
+ *
+ * This file is part of the Geneva library collection.
+ *
+ * Geneva was developed with kind support from Karlsruhe Institute of
+ * Technology (KIT) and Steinbuch Centre for Computing (SCC). Further
+ * information about KIT and SCC can be found at http://www.kit.edu/english
+ * and http://scc.kit.edu .
+ *
+ * Geneva is free software: you can redistribute and/or modify it under
+ * the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For further information on Gemfony scientific and Geneva, visit
+ * http://www.gemfony.com .
+ */
+
+/*
+ * This test adapts a double a given number of times and records the values of
+ * different entities of a GDoubleGaussAdaptor as a function of the iteration.
+ * The output can be processed with the help of the ROOT analysis toolkit (see
+ * http://root.cern.ch .
+ */
+
+// Standard header files go here
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+// Boost header files go here
+#include <boost/shared_ptr.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_io.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
+
+// Geneva header files go here
+#include "geneva/GDoubleBiGaussAdaptor.hpp"
+#include "common/GPlotDesigner.hpp"
+
+// Parsing of the command line
+#include "GCommandLineParser.hpp"
+
+using namespace Gem::Tests;
+using namespace Gem::Geneva;
+using namespace Gem::Common;
+using namespace Gem::Hap;
+
+int main(int argc, char **argv) {
+	bool verbose;
+	double sigma1, sigmaSigma1, minSigma1, maxSigma1;
+	double sigma2, sigmaSigma2, minSigma2, maxSigma2;
+	double delta, sigmaDelta, minDelta, maxDelta;
+	boost::uint32_t maxIter, adaptionThreshold;
+	std::string resultFile;
+
+	if (!parseCommandLine(argc, argv
+		                  , sigma1
+		                  , sigmaSigma1
+		                  , minSigma1
+		                  , maxSigma1
+		                  , sigma2
+		                  , sigmaSigma2
+		                  , minSigma2
+		                  , maxSigma2
+		                  , delta
+		                  , sigmaDelta
+		                  , minDelta
+		                  , maxDelta
+		                  , adaptionThreshold
+		                  , resultFile
+		                  , maxIter
+		                  , verbose
+		                  ))
+	{ exit(1); }
+
+	// The adaptor object to be tested
+	boost::shared_ptr<GDoubleBiGaussAdaptor> gdbga_ptr(new GDoubleBiGaussAdaptor());
+
+	gdbga_ptr->setUseSymmetricSigmas(false);
+	gdbga_ptr->setSigma1(sigma1);
+	gdbga_ptr->setSigma2(sigma2);
+	gdbga_ptr->setSigma1AdaptionRate(sigmaSigma1);
+	gdbga_ptr->setSigma2AdaptionRate(sigmaSigma2);
+	gdbga_ptr->setSigma1Range(minSigma1,maxSigma1);
+	gdbga_ptr->setSigma2Range(minSigma2,maxSigma2);
+	gdbga_ptr->setDeltaAdaptionRate(sigmaDelta);
+	gdbga_ptr->setDeltaRange(minDelta, maxDelta);
+	gdbga_ptr->setDelta(delta);
+	gdbga_ptr->setAdaptionThreshold(adaptionThreshold);
+
+	// Create the GPlotDesigner object
+	GPlotDesigner gpd("GDoubleBiGaussAdaptor Tests", 2,5);
+	gpd.setCanvasDimensions(1200,1200);
+
+	// 2D Graph for the value of the mutation-subject
+	boost::shared_ptr<GGraph2D> gmutpar_iter_ptr(new GGraph2D());
+	gmutpar_iter_ptr->setPlotMode(Gem::Common::SCATTER);
+	gmutpar_iter_ptr->setPlotLabel("Mutation parameter (iteration)");
+	gmutpar_iter_ptr->setXAxisLabel("Iteration");
+	gmutpar_iter_ptr->setYAxisLabel("Value of mutation parameter");
+
+	// 2D Graph for the difference between current and last mutation parameter
+	boost::shared_ptr<GGraph2D> gmutpardiff_iter_ptr(new GGraph2D());
+	gmutpardiff_iter_ptr->setPlotMode(Gem::Common::SCATTER);
+	gmutpardiff_iter_ptr->setPlotLabel("Difference between consecutive mutation parameters (iteration)");
+	gmutpardiff_iter_ptr->setXAxisLabel("Iteration");
+	gmutpardiff_iter_ptr->setYAxisLabel("Difference");
+
+	// 2D Graph for sigma1 as a function of the iteration
+	boost::shared_ptr<GGraph2D> gsigma1_iter_ptr(new GGraph2D());
+	gsigma1_iter_ptr->setPlotMode(Gem::Common::SCATTER);
+	gsigma1_iter_ptr->setPlotLabel("Sigma1 as a function of the iteration");
+	gsigma1_iter_ptr->setXAxisLabel("Iteration");
+	gsigma1_iter_ptr->setYAxisLabel("Sigma1");
+
+	// 2D Graph for sigma2 as a function of the iteration
+	boost::shared_ptr<GGraph2D> gsigma2_iter_ptr(new GGraph2D());
+	gsigma2_iter_ptr->setPlotMode(Gem::Common::SCATTER);
+	gsigma2_iter_ptr->setPlotLabel("Sigma2 as a function of the iteration");
+	gsigma2_iter_ptr->setXAxisLabel("Iteration");
+	gsigma2_iter_ptr->setYAxisLabel("Sigma2");
+
+	// 2D Graph for delta as a function of the iteration
+	boost::shared_ptr<GGraph2D> gdelta_iter_ptr(new GGraph2D());
+	gdelta_iter_ptr->setPlotMode(Gem::Common::SCATTER);
+	gdelta_iter_ptr->setPlotLabel("Delta as a function of the iteration");
+	gdelta_iter_ptr->setXAxisLabel("Iteration");
+	gdelta_iter_ptr->setYAxisLabel("Delta");
+
+
+	// Fill the objects with data
+	double mutVal = 0.;
+	double mutValOld=0;
+	for (boost::uint32_t i = 0; i < maxIter; i++) {
+		mutValOld = mutVal;
+
+		gdbga_ptr->adapt(mutVal);
+
+		(*gmutpar_iter_ptr) & boost::tuple<double, double>((double)i, mutVal); // The new value of sigma
+		(*gmutpardiff_iter_ptr) & boost::tuple<double, double>((double)i, mutVal - mutValOld); // Difference between last known value and current value
+		(*gsigma1_iter_ptr) & boost::tuple<double, double>((double)i, gdbga_ptr->getSigma1()); // Value of sigma1 after sigma-adaption
+		(*gsigma2_iter_ptr) & boost::tuple<double, double>((double)i, gdbga_ptr->getSigma2()); // Value of sigma2 after sigma-adaption
+		(*gdelta_iter_ptr) & boost::tuple<double, double>((double)i, gdbga_ptr->getDelta()); // Value of delta after adaption
+	}
+
+	// Register the plots (and projections) with the plot designer
+	gpd.registerPlotter(gmutpar_iter_ptr);
+	gpd.registerPlotter(gmutpar_iter_ptr->projectY());
+	gpd.registerPlotter(gmutpardiff_iter_ptr);
+	gpd.registerPlotter(gmutpardiff_iter_ptr->projectY());
+	gpd.registerPlotter(gsigma1_iter_ptr);
+	gpd.registerPlotter(gsigma1_iter_ptr->projectY());
+	gpd.registerPlotter(gsigma2_iter_ptr);
+	gpd.registerPlotter(gsigma2_iter_ptr->projectY());
+	gpd.registerPlotter(gdelta_iter_ptr);
+	gpd.registerPlotter(gdelta_iter_ptr->projectY());
+
+	// Write the result to disk
+	gpd.writeToFile("result.C");
+
+	return 0;
+}
---
+++ tests/geneva/ManualTests/GFloatBiGaussAdaptorTest/GCommandLineParser.cpp
@@ -0,0 +1,145 @@
+/**
+ * @file GCommandLineParser.cpp
+ */
+
+/*
+ * Copyright (C) Gemfony scientific UG (haftungsbeschraenkt)
+ *
+ * See the AUTHORS file in the top-level directory for a list of authors.
+ *
+ * Contact: contact [at] gemfony (dot) com
+ *
+ * This file is part of the Geneva library collection.
+ *
+ * Geneva was developed with kind support from Karlsruhe Institute of
+ * Technology (KIT) and Steinbuch Centre for Computing (SCC). Further
+ * information about KIT and SCC can be found at http://www.kit.edu/english
+ * and http://scc.kit.edu .
+ *
+ * Geneva is free software: you can redistribute and/or modify it under
+ * the terms of version 3 of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * Geneva is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Geneva library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For further information on Gemfony scientific and Geneva, visit
+ * http://www.gemfony.com .
+ */
+
+#include "GCommandLineParser.hpp"
+
+namespace Gem {
+namespace Tests {
+
+/************************************************************************************************/
+/**
+ * A function that parses the command line for all required parameters
+ */
+
+bool parseCommandLine (
+		int argc, char **argv
+		, float& sigma1
+		, float& sigmaSigma1
+		, float& minSigma1
+		, float& maxSigma1
+		, float& sigma2
+		, float& sigmaSigma2
+		, float& minSigma2
+		, float& maxSigma2
+		, float& delta
+		, float& sigmaDelta
+		, float& minDelta
+		, float& maxDelta
+		, boost::uint32_t& adaptionThreshold
+		, std::string& resultFile
+		, boost::uint32_t& maxIter
+		, bool& verbose
+){
+	try {
+		// Check the command line options. Uses the Boost program options library.
+		po::options_description desc("Allowed options");
+		desc.add_options()
+		("help,h", "emit help message")
+		("sigma1",po::value<float>(&sigma1)->default_value(CMD_DEFAULTSIGMA1),
+				"Width of the first gaussian")
+		("sigmaSigma1",po::value<float>(&sigmaSigma1)->default_value(CMD_DEFAULTSIGMASIGMA1),
+				"Width of the gaussian used to adapt sigma1")
+		("minSigma1",po::value<float>(&minSigma1)->default_value(CMD_DEFAULTMINSIGMA1),
+				"Minimal allowed value of sigma1")
+		("maxSigma1",po::value<float>(&maxSigma1)->default_value(CMD_DEFAULTMAXSIGMA1),
+				"Maximum allowed value of sigma1")
+		("sigma2",po::value<float>(&sigma2)->default_value(CMD_DEFAULTSIGMA2),
+				"Width of the second gaussian")
+		("sigmaSigma2",po::value<float>(&sigmaSigma2)->default_value(CMD_DEFAULTSIGMASIGMA2),
+				"Width of the gaussian used to adapt sigma2")
+		("minSigma2",po::value<float>(&minSigma2)->default_value(CMD_DEFAULTMINSIGMA2),
+				"Minimal allowed value of sigma2")
+		("maxSigma2",po::value<float>(&maxSigma2)->default_value(CMD_DEFAULTMAXSIGMA2),
+				"Maximum allowed value of sigma2")
+		("delta",po::value<float>(&delta)->default_value(CMD_DEFAULTDELTA),
+				"Distance between both gaussians")
+		("sigmaDelta",po::value<float>(&sigmaDelta)->default_value(CMD_DEFAULTSIGMADELTA),
+				"Width of the gaussian used to adapt delta")
+		("minDelta",po::value<float>(&minDelta)->default_value(CMD_DEFAULTMINDELTA),
+				"Minimal allowed value for delta")
+		("maxDelta",po::value<float>(&maxDelta)->default_value(CMD_DEFAULTMAXDELTA),
+				"Maximum allowed value for delta")
+		("adaptionThreshold,a",po::value<boost::uint32_t>(&adaptionThreshold)->default_value(CMD_DEFAULTADAPTIONTHRESHOLD),
+				"Number of calls to adapt() after which the adaption parameters should be modified")
+		("resultFile,F",po::value<std::string>(&resultFile)->default_value(CMD_DEFAULTRESULTFILE),
+				"The file to write the result to")
+		("maxIter,I",po::value<boost::uint32_t>(&maxIter)->default_value(CMD_DEFAULTMAXITER),
+				"The maximum number of test cycles")
+		("verbose,v",po::value<bool>(&verbose)->default_value(CMD_DEFAULTVERBOSE),
+				"Whether to emit status information")
+		;
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		// Emit a help message, if necessary
+		if (vm.count("help")) {
+			std::cout << desc << std::endl;
+			return false;
+		}
+
+		if(verbose) {
+			std::cout << std::endl
+			<< "Running with the following options:" << std::endl
+			<< "sigma1 = " << sigma1 << std::endl
+			<< "sigmaSigma1 = " << sigmaSigma1 << std::endl
+			<< "minSigma1 = " << minSigma1 << std::endl
+			<< "maxSigma1 = " << maxSigma1 << std::endl
+			<< "sigma2 = " << sigma1 << std::endl
+			<< "sigmaSigma2 = " << sigmaSigma1 << std::endl
+			<< "minSigma2 = " << minSigma1 << std::endl
+			<< "maxSigma2 = " << maxSigma1 << std::endl
+			<< "delta = " << delta << std::endl
+			<< "sigmaDelta = " << sigmaDelta << std::endl
+			<< "minDelta = " << minDelta << std::endl
+			<< "maxDelta = " << maxDelta << std::endl
+			<< "adaptionThreshold = " << adaptionThreshold << std::endl
+			<< "resultFile = " << resultFile << std::endl
+			<< "maxIter = " << maxIter << std::endl
+			<< std::endl;
+		}
+	}
+	catch(...) {
+		std::cout << "Error parsing the command line" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+/************************************************************************************************/
+
+} /* namespace Tests */
+} /* namespace Gem */
