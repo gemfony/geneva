e02870ac78ac8e499a3d7ea552e2730a47a4f95f
rberlich <Unknown>
2014-03-04 10:38:29 +0100

    Update of configuration parameters for example 7

--- examples/geneva/07_GFunctionMinimizer/GFMinIndividual.hpp
+++ examples/geneva/07_GFunctionMinimizer/GFMinIndividual.hpp
@@ -83,7 +83,7 @@ std::istream& operator>>(std::istream&, Gem::Geneva::targetFunction&);
 // A number of default settings for the factory
 const double GFI_DEF_ADPROB = 1.0;
 const double GFI_DEF_SIGMA = 0.025;
-const double GFI_DEF_SIGMASIGMA = 0.8;
+const double GFI_DEF_SIGMASIGMA = 0.2;
 const double GFI_DEF_MINSIGMA = 0.001;
 const double GFI_DEF_MAXSIGMA = 1;
 const std::size_t GFI_DEF_PARDIM = 2;
