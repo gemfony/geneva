b115bbabc75203eb4979c4ce260ceda19a32537d
rberlich <Unknown>
2014-05-02 12:30:27 +0200

    Seperated .h and .cpp for GSigmaMonitor in example 7 in order to ease the discussion in the Geneva manual

--- examples/geneva/07_GFunctionMinimizer/GSigmaMonitor.hpp
+++ examples/geneva/07_GFunctionMinimizer/GSigmaMonitor.hpp
@@ -82,148 +82,29 @@ class GSigmaMonitor
    ///////////////////////////////////////////////////////////////////////
 
 public:
-   /***************************************************************************/
-   /**
-    * The default constructor
-    */
-   GSigmaMonitor(const std::string fileName)
-   : GBaseEA::GEAOptimizationMonitor()
-   , fileName_(fileName)
-   , gpd_("Progress information", 1, 2)
-   , progressPlotter_(new Gem::Common::GGraph2D())
-   , sigmaPlotter_(new Gem::Common::GGraph2D())
-   { /* nothing */ }
+   /** @brief The default constructor */
+   GSigmaMonitor(const std::string fileName);
+   /** @brief The copy constructor */
+   GSigmaMonitor(const GSigmaMonitor& cp);
+   /** @brief The destructor */
+   virtual ~GSigmaMonitor();
 
-   /***************************************************************************/
-   /**
-    * The copy constructor
-    *
-    * @param cp A copy of another GSigmaMonitor object
-    */
-   GSigmaMonitor(const GSigmaMonitor& cp)
-   : GBaseEA::GEAOptimizationMonitor(cp)
-   , fileName_(cp.fileName_)
-   , gpd_("Progress information", 1, 2) // We do not want to copy progress information of another object
-   , progressPlotter_(new Gem::Common::GGraph2D())
-   , sigmaPlotter_(new Gem::Common::GGraph2D())
-   { /* nothing */ }
+protected:
+   /** @brief A function that is called once before the optimization starts */
+   virtual void firstInformation(GOptimizationAlgorithmT<GParameterSet> * const goa);
+   /** @brief A function that is called during each optimization cycle */
+   virtual void cycleInformation(GOptimizationAlgorithmT<GParameterSet> * const goa);
+   /** @brief A function that is called once at the end of the optimization cycle */
+   virtual void lastInformation(GOptimizationAlgorithmT<GParameterSet> * const goa);
 
-   /***************************************************************************/
-   /**
-    * The destructor
-    */
-   virtual ~GSigmaMonitor()
-   { /* nothing */ }
-
-       protected:
-   /********************************************************************************************/
-   /**
-    * A function that is called once before the optimization starts
-    *
-    * @param cp A constant pointer to the evolutionary algorithm that is calling us
-    */
-   virtual void firstInformation(GOptimizationAlgorithmT<GParameterSet> * const goa) {
-      // Initialize the plots we want to record
-      progressPlotter_->setPlotMode(Gem::Common::CURVE);
-      progressPlotter_->setPlotLabel("Fitness as a function of the iteration");
-      progressPlotter_->setXAxisLabel("Iteration");
-      progressPlotter_->setYAxisLabel("Best Result (lower is better)");
-
-      sigmaPlotter_->setPlotMode(Gem::Common::CURVE);
-      sigmaPlotter_->setPlotLabel("Development of sigma (aka \\\"step width\\\")");
-      sigmaPlotter_->setXAxisLabel("Iteration");
-      sigmaPlotter_->setYAxisLabel("Sigma");
-
-      gpd_.setCanvasDimensions(P_XDIM, P_YDIM);
-      gpd_.registerPlotter(progressPlotter_);
-      gpd_.registerPlotter(sigmaPlotter_);
-
-      // We call the parent classes firstInformation function,
-      // as we do not want to change its actions
-      GBaseEA::GEAOptimizationMonitor::firstInformation(goa);
-   }
-
-   /********************************************************************************************/
-   /**
-    * A function that is called during each optimization cycle. The function first collects
-    * the requested data, then calls the parent class'es eaCycleInformation() function, as
-    * we do not want to change its actions.
-    *
-    * @param cp A constant pointer to the evolutionary algorithm that is calling us
-    */
-   virtual void cycleInformation(GOptimizationAlgorithmT<GParameterSet> * const goa) {
-      // Convert the base pointer to the target type
-      GBaseEA * const ea = static_cast<GBaseEA * const>(goa);
-
-      // Extract the requested data. First retrieve the best individual.
-      // It can always be found in the first position with evolutionary algorithms
-      boost::shared_ptr<GFMinIndividual> p = ea->clone_at<GFMinIndividual>(0);
-
-      // Retrieve the best "raw" fitness and average sigma value and add it to our local storage
-      progressPlotter_->add(boost::tuple<double,double>((double)ea->getIteration(), p->fitness()));
-      sigmaPlotter_->add(boost::tuple<double,double>((double)ea->getIteration(), p->getAverageSigma()));
-      //---------------------------------------------------------
-      // Call our parent class'es function
-      GBaseEA::GEAOptimizationMonitor::cycleInformation(goa);
-   }
-
-   /***************************************************************************/
-   /**
-    * A function that is called once at the end of the optimization cycle
-    *
-    * @param cp A constant pointer to the evolutionary algorithm that is calling us
-    */
-   virtual void lastInformation(GOptimizationAlgorithmT<GParameterSet> * const goa) {
-      // Write out the result
-      gpd_.writeToFile(fileName_);
-
-      // We just call the parent classes eaLastInformation function,
-      // as we do not want to change its actions
-      GBaseEA::GEAOptimizationMonitor::lastInformation(goa);
-   }
-
-   /********************************************************************************************/
-   /**
-    * Loads the data of another object
-    *
-    * @param cp A copy of another GSigmaMonitor object, camouflaged as a GObject
-    */
-   virtual void load_(const GObject* cp) {
-      // Check that we are indeed dealing with an object of the same type and that we are not
-      // accidently trying to compare this object with itself.
-      const GSigmaMonitor *p_load = gobject_conversion<GSigmaMonitor>(cp);
-
-      // Trigger loading of our parent class'es data
-      GBaseEA::GEAOptimizationMonitor::load_(cp);
-
-      // Load local data
-      fileName_ = p_load->fileName_;
-   }
-
-   /***************************************************************************/
-   /**
-    * Creates a deep clone of this object
-    *
-    * @return A deep clone of this object
-    */
-   virtual GObject* clone_() const {
-      return new GSigmaMonitor(*this);
-   }
+   /** @brief Loads the data of another object */
+   virtual void load_(const GObject* cp);
+   /** @brief Creates a deep clone of this object */
+   virtual GObject* clone_() const;
 
 private:
-   /***************************************************************************/
-   /**
-    * The default constructor -- intentionally private
-    */
-   GSigmaMonitor()
-      : GBaseEA::GEAOptimizationMonitor()
-      , fileName_("empty")
-      , gpd_("Progress information", 1, 2)
-      , progressPlotter_(new Gem::Common::GGraph2D())
-      , sigmaPlotter_(new Gem::Common::GGraph2D())
-   { /* nothing */ }
-
-   /***************************************************************************/
+   /** @brief The default constructor -- intentionally private */
+   GSigmaMonitor();
 
    std::string fileName_; ///< The name of the output file
 
