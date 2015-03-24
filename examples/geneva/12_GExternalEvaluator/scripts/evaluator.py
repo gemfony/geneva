dd2104024fa101b05557c8c3a6a8f66e494882df
Ariel Garcia <a.garcia@gemfony.eu>
2015-03-24 23:47:42 +0100

    Update for matching GExternalEvaluatorIndividual

--- examples/geneva/12_GExternalEvaluator/scripts/evaluator.py
+++ examples/geneva/12_GExternalEvaluator/scripts/evaluator.py
@@ -33,11 +33,12 @@
 ################################################################################
 
 import argparse
+import os
 import sys
-from os import path
-from textwrap import dedent
-from datetime import datetime
 import xml.etree.ElementTree as ET
+
+from datetime import datetime
+from textwrap import dedent
 
 
 def init():
@@ -58,100 +59,126 @@ def setup(setup_file, initial_values):
     """
     Provide the problem setup data inside a file of the given name.
     """
+
+    # We write the XML "by-hand", as ElementTree does not support "pretty-printing"
     header = """\
     <!--
         Problem setup file created by """ + command_name() + """
         on """ + current_time() + """
     -->
-    <parameterset>
-        <nResultsExpected>1</nResultsExpected>
-        <nvar>4</nvar>
-        <type>GParameterSet</type>\n"""
+    <batch>
+      <dataType>setup_data</dataType>
+      <runID>0</runID>
+      <nIndividuals>1</nIndividuals>
+      <individuals>
+        <individual0>
+          <type>GParameterSet</type>
+          <nVars>4</nVars>
+          <vars>\n"""
 
     content = header
     for i in range(4):
-        content += "        <var" + str(i) + ">\n"
-        content += "            <nvar>1</nvar>\n"
-        content += "            <type>GConstrainedDoubleObject</type>\n"
-        content += "            <baseType>double</baseType>\n"
-        content += "            <isLeaf>true</isLeaf>\n"
-        content += "            <name>var" + str(i) + "</name>\n"
+        content += "            <var" + str(i) + ">\n"
+        content += "              <name>coord_" + str(i) + "</name>\n"
+        content += "              <type>GConstrainedDoubleObject</type>\n"
+        content += "              <baseType>double</baseType>\n"
+        content += "              <isLeaf>true</isLeaf>\n"
+        content += "              <nVals>1</nVals>\n"
         if initial_values == "min":
-            content += "            <value0>-10.0</value0>\n"
-            content += "            <lowerBoundary>-10.0</lowerBoundary>\n"
-            content += "            <upperBoundary>10.0</upperBoundary>\n"
-            content += "            <initRandom>false</initRandom>\n"
+            content += "              <values>\n"
+            content += "                <value0>-10.0</value0>\n"
+            content += "              </values>\n"
+            content += "              <lowerBoundary>-10.0</lowerBoundary>\n"
+            content += "              <upperBoundary>10.0</upperBoundary>\n"
+            content += "              <initRandom>false</initRandom>\n"
         elif initial_values == "max":
-            content += "            <value0>10.0</value0>\n"
-            content += "            <lowerBoundary>-10.0</lowerBoundary>\n"
-            content += "            <upperBoundary>10.0</upperBoundary>\n"
-            content += "            <initRandom>false</initRandom>\n"
+            content += "              <values>\n"
+            content += "                <value0>10.0</value0>\n"
+            content += "              </values>\n"
+            content += "              <lowerBoundary>-10.0</lowerBoundary>\n"
+            content += "              <upperBoundary>10.0</upperBoundary>\n"
+            content += "              <initRandom>false</initRandom>\n"
         else:
-            content += "            <value0>0.0</value0>\n"
-            content += "            <lowerBoundary>-10.0</lowerBoundary>\n"
-            content += "            <upperBoundary>10.0</upperBoundary>\n"
-            content += "            <initRandom>true</initRandom>\n"
-        content += "        </var" + str(i) + ">\n"
+            content += "              <values>\n"
+            content += "                <value0>0.0</value0>\n"
+            content += "              </values>\n"
+            content += "              <lowerBoundary>-10.0</lowerBoundary>\n"
+            content += "              <upperBoundary>10.0</upperBoundary>\n"
+            content += "              <initRandom>true</initRandom>\n"
+        content += "          </var" + str(i) + ">\n"
 
     content += """\
-    </parameterset>
+          </vars>
+          <nBounds>0</nBounds>
+          <nResults>1</nResults>
+        </individual0>
+      </individuals>
+    </batch>
     """
 
-    setup_file.write(dedent(content))
-    setup_file.close()
+    with open(setup_file, 'w') as infile:
+        infile.write(dedent(content))
 
-def evaluate_result(in_file, out_file):
+def evaluate(in_file, out_file):
     """
     Evaluate the result for the given input parameters, and
     generate an output file.
     """
-    (iteration, result) = evaluate(in_file)
-    write_output(out_file, iteration, result)
+    (iteration, it_id, result) = process_input(in_file)
+    write_output(out_file, iteration, it_id, result)
 
-def evaluate_archive(in_file):
+def archive(in_file):
     """
     Evaluate the result for the given input parameters, and
     archive the results.
     """
-    (iteration, result) = evaluate(in_file)
-    archive_output(iteration, result)
+    (iteration, it_id, result) = process_input(in_file)
+    archive_output(iteration, it_id, result)
 
-def evaluate(in_file):
+def process_input(in_file):
     """
-    Evaluate the result for the given input parameters.
+    Read the input data and process it to get the results.
     """
-    # print("Evaluating result for the given parameters...")
-    (iteration, x, y, z, w) = read_input(in_file)
+    (iteration, it_id, x, y, z, w) = read_input(in_file)
 
-    # ATM we just calculate a 4D parabola...
+    # This is an example, we just calculate a 4D parabola...
     result = x**2 + y**2 + w**2 + z**2
 
-    #cmd = 'executable' + 'arguments' 
-    #os.system(cmd)
+    return (iteration, it_id, result)
 
-    return (iteration, result)
-
-def write_output(out_file, iteration, result):
+def write_output(out_file, iteration, it_id, result):
     """
     Write the output file containing the calculated results.
     """
-    # print("Generating the output file...")
+    # We write the XML "by-hand", as ElementTree does not support "pretty-printing"
     header = """\
     <!--
         Results file created by """ + command_name() + """
         on """ + current_time() + """
         for iteration """ + str(iteration) + """
     -->
-    <results>
-        <isUseful>true</isUseful>
-        <nresult>1</nresult>
-        <result0>""" + str(result) + """</result0>
-    </results>
+    <batch>
+      <dataType>run_results</dataType>
+      <runID>0</runID>
+      <nIndividuals>1</nIndividuals>
+      <individuals>
+        <individual0>
+          <iteration>""" + str(iteration) + """</iteration>
+          <id>""" + str(it_id) + """</id>
+          <isValid>true</isValid>
+          <isDirty>false</isDirty>
+          <nResults>1</nResults>
+          <results>
+            <rawResult0>""" + str(result) + """</rawResult0>
+          </results>
+        </individual0>
+      </individuals>
+    </batch>
     """
-    out_file.write(dedent(header))
-    out_file.close()
+    with open(out_file, 'w') as ofile:
+        ofile.write(dedent(header))
 
-def archive_output(iteration, result):
+def archive_output(iteration, it_id, result):
     """
     Archive the calculated results (for instance, this could mean
     to store the results in a DB).
@@ -166,7 +193,8 @@ def read_input(in_file):
 
     xmltree = None
     try:
-        xmltree = ET.parse(in_file)
+        with open(in_file) as ifile:
+            xmltree = ET.parse(ifile)
     except ET.ParseError as e:
         sys.exit("\nERROR: parsing error on input file '"
                  + in_file.name + "':\n\t" + str(e))
@@ -176,26 +204,36 @@ def read_input(in_file):
     # More error handling needed here, in case the XML file
     # doesn't have the expected structure
     #
-    iteration = int(root.find("./iteration").text)
-    # nr_results = int(root.find("./nResultsExpected").text)
-    nr_params = int(root.find("./nvar").text)
+    ind = root.find("./individuals/individual0")
+    try:
+        iteration = int(ind.find("./iteration").text)
+    except:
+        iteration = -1
+
+    try:
+        it_id = ind.find("./id").text
+    except:
+        it_id = "UNKNOWN_ID"
+
+    # nr_results = int(root.find("./nResults").text)
+    nr_params = int(ind.find("./nVars").text)
 
     # if nr_results != 1:
-    #    sys.exit("\nERROR: unexpected parameter: nResultsExpected=" + str(nr_results) + " (expected 1)!")
+    #    sys.exit("\nERROR: unexpected parameter: nResults=" + str(nr_results) + " (expected 1)!")
     if nr_params != 4:
-        sys.exit("\nERROR: unexpected parameter: nvar=" + str(nr_params) + " (expected 4)!")
+        sys.exit("\nERROR: unexpected parameter: nVars=" + str(nr_params) + " (expected 4)!")
 
-    params = root.findall(".//*[isLeaf]")
+    params = ind.findall(".//*[isLeaf]")
     if len(params) != nr_params:
-        sys.exit("\nERROR: inconsistent data in input file: nvar=" + str(nr_params)
+        sys.exit("\nERROR: inconsistent data in input file: nVars=" + str(nr_params)
                  + ", but found " + str(len(params)) + " parameters!")
 
-    x = float(root.find("./var0").find("value0").text)
-    y = float(root.find("./var1").find("value0").text)
-    z = float(root.find("./var2").find("value0").text)
-    w = float(root.find("./var3").find("value0").text)
+    x = float(ind.find("./vars/var0").find("values/value0").text)
+    y = float(ind.find("./vars/var1").find("values/value0").text)
+    z = float(ind.find("./vars/var2").find("values/value0").text)
+    w = float(ind.find("./vars/var3").find("values/value0").text)
 
-    return (iteration, x, y, z, w)
+    return (iteration, it_id, x, y, z, w)
 
 
 def main(argv):
@@ -216,29 +254,24 @@ def main(argv):
     group.add_argument("--finalize", help="Perform finalization work",
                        action="store_true")
     group.add_argument("--setup", help="Provide initialization data to the caller",
-                       nargs='?', metavar='SETUP_FILE', const="setupFile.xml",
-                       type=argparse.FileType('w'))
-    group.add_argument("--evaluate", help="Evaluate the parameters from the input file"
-                       " (requires --result or --archive)",
-                       nargs='?', metavar='IN_FILE', const="parameters.xml",
-                       type=argparse.FileType('r'))
+                       action="store_true")
+    group.add_argument("--evaluate", help="Evaluate the parameters from the input file",
+                       action="store_true")
+    group.add_argument("--archive", help="Archive the results",
+                       action="store_true")
 
-    group_eval = parser.add_mutually_exclusive_group()
-    group_eval.add_argument("--result", help="Write the results to the given file",
-                            nargs='?', metavar='OUT_FILE', const="results.xml",
-                            type=argparse.FileType('w'))
-    group_eval.add_argument("--archive", help="Archive the results",
-                            action="store_true")
+    parser.add_argument("--data", help="Read the input data from the given file"
+                        " (default: 'data.xml')",
+                        metavar='IN_FILE', default="data.xml")
+    parser.add_argument("--result", help="Write the results to the given file"
+                        " (default: 'output.xml')",
+                        metavar='OUT_FILE', default="output.xml")
 
     args = parser.parse_args()
 
     # These options checks are not supported by argparse (in Python 2.7 at least)
-    if args.result or args.archive:
-        if args.evaluate is None:
-            sys.exit('--result and --archive can only be used with --evaluate! Aborting...')
-    else:
-        if args.evaluate:
-            sys.exit('--evaluate requires either --result or --archive! Aborting...')
+    if args.initvalues and not args.setup:
+        sys.exit('The option --initvalues may only be used with --setup! Aborting...')
 
     initial_values = args.initvalues or "random"
     if initial_values not in set(["min", "max", "random"]):
@@ -250,21 +283,19 @@ def main(argv):
     elif args.finalize:
         finalize()
     elif args.setup:
-        setup(args.setup, initial_values)
+        print(repr(args.setup))
+        setup(args.result, initial_values)
     elif args.evaluate:
-        if args.result:
-            evaluate_result(args.evaluate, args.result)
-        elif args.archive:
-            evaluate_archive(args.evaluate)
-        else:
-            sys.exit("ERROR: inconsistent options! why are we here?")
+        evaluate(args.data, args.result)
+    elif args.archive:
+        archive(args.data)
     else:
         sys.exit("ERROR: inconsistent options! why are we here?")
 
 
 def command_name():
     """Return a cleaned up name of this program."""
-    return path.basename(sys.argv[0])
+    return os.path.basename(sys.argv[0])
 
 def current_time():
     """Return the current date/time as a string."""
