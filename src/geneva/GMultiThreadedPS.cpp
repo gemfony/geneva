374ac3f6fb6ccf9a2633ea3890d3bb5bd20b56a1
unknown <r.berlich@gemfony.com>
2015-04-22 01:25:37 +0200

    Working towards removal of all boost::bind calls

--- src/geneva/GMultiThreadedPS.cpp
+++ src/geneva/GMultiThreadedPS.cpp
@@ -261,26 +261,17 @@ void GMultiThreadedPS::finalize() {
 void GMultiThreadedPS::addConfigurationOptions (
    Gem::Common::GParserBuilder& gpb
 ) {
-   std::string comment;
-
    // Call our parent class'es function
    GBasePS::addConfigurationOptions(gpb);
 
    // add local data
-   comment = ""; // Reset the comment string
-   comment += "The number of evaluation threads;";
-   comment += "0 means: determine automatically;";
    gpb.registerFileParameter<boost::uint16_t>(
       "nEvaluationThreads" // The name of the variable
       , 0 // The default value
-      , boost::bind(
-         &GMultiThreadedPS::setNThreads
-         , this
-         , _1
-      )
-      , Gem::Common::VAR_IS_ESSENTIAL // Alternative: VAR_IS_SECONDARY
-      , comment
-   );
+      , [this](boost::uint16_t nt){ this->setNThreads(nt); }
+   )
+   << "The number of evaluation threads" << std::endl
+   << "0 means: determine automatically;";
 }
 
 /******************************************************************************/
@@ -317,15 +308,7 @@ void GMultiThreadedPS::runFitnessCalculation() {
 
       // Do the actual scheduling
       tp_ptr_->async_schedule(
-         boost::function<double()>(
-            boost::bind(
-               &GParameterSet::nonConstFitness
-               , *it
-               , 0
-               , Gem::Geneva::ALLOWREEVALUATION
-               , Gem::Geneva::USETRANSFORMEDFITNESS
-            )
-         )
+         [it](){ (*it)->nonConstFitness(0, ALLOWREEVALUATION, USETRANSFORMEDFITNESS); }
       );
    }
 
