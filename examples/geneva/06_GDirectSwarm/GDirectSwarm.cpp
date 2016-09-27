378a0c8ab2ac26cbc2fa5abf0eed7b99f63cd001
rberlich <r.berlich@gemfony.eu>
2016-09-27 20:20:07 +0200

    Removed an obsolete argument parser fole

--- examples/geneva/06_GDirectSwarm/GDirectSwarm.cpp
+++ examples/geneva/06_GDirectSwarm/GDirectSwarm.cpp
@@ -62,9 +62,6 @@
 // The individual that should be optimized
 #include "geneva-individuals/GFunctionIndividual.hpp"
 
-// Declares a function to parse the command line
-#include "GArgumentParser.hpp"
-
 // Holds the optimization monitor
 #include "GInfoFunction.hpp"
 
@@ -72,6 +69,253 @@ using namespace Gem::Geneva;
 using namespace Gem::Courtier;
 using namespace Gem::Hap;
 using namespace Gem::Common;
+
+namespace po = boost::program_options;
+
+/******************************************************************************/
+
+const execMode DEFAULTPARALLELIZATIONMODEAP=execMode::EXECMODE_MULTITHREADED;
+const unsigned short DEFAULTPORT=10000;
+const std::string DEFAULTIP="localhost";
+const std::uint32_t DEFAULTMAXSTALLS06=0;
+const std::uint32_t DEFAULTMAXCONNECTIONATTEMPTS06=100;
+const std::uint16_t DEFAULTNPRODUCERTHREADS=10;
+const Gem::Common::serializationMode DEFAULTSERMODE=Gem::Common::serializationMode::SERIALIZATIONMODE_TEXT;
+const bool DEFAULTADDLOCALCONSUMER=false;
+const std::uint16_t DEFAULTNEVALUATIONTHREADS=4;
+const std::uint32_t DEFAULTMAXITERATIONS=200;
+const std::uint32_t DEFAULTREPORTITERATION=1;
+const long DEFAULTMAXMINUTES=10;
+const std::uint16_t DEFAULTXDIMAP=1024;
+const std::uint16_t DEFAULTYDIMAP=1024;
+const bool DEFAULTFOLLOWPROGRESS=false;
+const std::size_t DEFAULTNNEIGHBORHOODSAP=5;
+const std::size_t DEFAULTNNEIGHBORHOODMEMBERSAP=20;
+const double DEFAULTCPERSONALAP=2.;
+const double DEFAULTCNEIGHBORHOODAP=2.;
+const double DEFAULTCGLOBALAP=1.;
+const double DEFAULTCVELOCITYAP=0.4;
+const bool DEFAULTALLRANDOMINIT=true;
+
+/******************************************************************************/
+/**
+ * Parses the command line
+ */
+bool parseCommandLine(
+	int argc, char **argv
+	, execMode& parallelizationMode
+	, bool& serverMode
+	, std::string& ip
+	, unsigned short& port
+	, std::uint32_t& maxStalls
+	, std::uint32_t& maxConnectionAttempts
+	, Gem::Common::serializationMode& serMode
+	, bool& addLocalConsumer
+	, std::size_t& nNeighborhoods
+	, std::size_t& nNeighborhoodMembers
+	, double& cPersonal
+	, double& cNeighborhood
+	, double& cGlobal
+	, double& cVelocity
+	, updateRule& ur
+	, bool& allRandomInit
+	, std::uint16_t& nProducerThreads
+	, std::uint16_t& nEvaluationThreads
+	, std::uint32_t& maxIterations
+	, long& maxMinutes
+	, std::uint32_t& reportIteration
+	, std::uint16_t& xDim
+	, std::uint16_t& yDim
+	, bool& followProgress
+){
+	// Create the parser builder
+	Gem::Common::GParserBuilder gpb;
+
+	gpb.registerCLParameter<execMode>(
+		"parallelizationMode,p"
+		, parallelizationMode
+		, DEFAULTPARALLELIZATIONMODEAP
+		, "Whether to run the optimization in serial (0), multi-threaded (1) or networked (2) mode"
+	);
+
+	gpb.registerCLParameter<bool>(
+		"serverMode,s"
+		, serverMode
+		, false // Use client mode, if no server option is specified
+		, "Whether to run networked execution in server or client mode. The option only has an effect if \"--parallelizationMode=2\". You can either say \"--server=true\" or just \"--server\"."
+		, GCL_IMPLICIT_ALLOWED // Permit implicit values, so that we can say --server instead of --server=true
+		, true // Use server mode, of only -s or --server was specified
+	);
+
+	gpb.registerCLParameter<std::string>(
+		std::string("ip")
+		, ip
+		, DEFAULTIP
+		, "The ip of the server"
+	);
+
+	gpb.registerCLParameter<unsigned short>(
+		"port"
+			, port
+			, DEFAULTPORT
+			, "The port on the server"
+	);
+
+	gpb.registerCLParameter<std::uint32_t>(
+		"maxStalls"
+		, maxStalls
+		, DEFAULTMAXSTALLS06
+		, "The number of stalled data transfers (i.e. transfers without a useful work item returned) before the client terminates in networked mode"
+	);
+
+	gpb.registerCLParameter<std::uint32_t>(
+		"maxConnectionAttempts"
+		, maxConnectionAttempts
+		, DEFAULTMAXCONNECTIONATTEMPTS06
+		, "The number of connection attempts from client to server, before the client terminates in networked mode"
+	);
+
+	gpb.registerCLParameter<Gem::Common::serializationMode>(
+		"serializationMode"
+		, serMode
+		, DEFAULTSERMODE
+		, "Specifies whether serialization shall be done in TEXTMODE (0), XMLMODE (1) or BINARYMODE (2)"
+	);
+
+	gpb.registerCLParameter<bool>(
+		"addLocalConsumer"
+		, addLocalConsumer
+		, DEFAULTADDLOCALCONSUMER // Use client mode, if no server option is specified
+		, "Whether or not a local consumer should be added to networked execution. You can use this option with or without arguments."
+		, GCL_IMPLICIT_ALLOWED // Permit implicit values, so that we can say --server instead of --server=true
+		, true // Use a local consumer if the option --addLocalConsumer was given without arguments
+	);
+
+	gpb.registerCLParameter<std::size_t>(
+		"nNeighborhoods"
+		, nNeighborhoods
+		, DEFAULTNNEIGHBORHOODSAP
+		, "The number of neighborhoods in the population"
+	);
+
+	gpb.registerCLParameter<std::size_t>(
+		"nNeighborhoodMembers"
+		, nNeighborhoodMembers
+		, DEFAULTNNEIGHBORHOODMEMBERSAP
+		, "The default number of members in each neighborhood"
+	);
+
+	gpb.registerCLParameter<double>(
+		"cPersonal"
+		, cPersonal
+		, DEFAULTCPERSONALAP
+		, "A constant to be multiplied with the personal direction vector"
+	);
+
+	gpb.registerCLParameter<double>(
+		"cNeighborhood"
+		, cNeighborhood
+		, DEFAULTCNEIGHBORHOODAP
+		, "A constant to be multiplied with the neighborhood direction vector"
+	);
+
+	gpb.registerCLParameter<double>(
+		"cGlobal"
+		, cGlobal
+		, DEFAULTCGLOBALAP
+		, "A constant to be multiplied with the global direction vector"
+	);
+
+	gpb.registerCLParameter<double>(
+		"cVelocity"
+		, cVelocity
+		, DEFAULTCVELOCITYAP
+		, "A constant to be multiplied with the old velocity vector"
+	);
+
+	gpb.registerCLParameter<updateRule>(
+		"updateRule"
+		, ur
+		, DEFAULTUPDATERULE
+		, "Use linear (0) or classical (1) update rule"
+	);
+
+	gpb.registerCLParameter<bool>(
+		"allRandomInit"
+		, allRandomInit
+		, DEFAULTALLRANDOMINIT
+		, "If set, all individuals will be initialized randomly. If 0, all individuals in one neighborhood will have the same start value"
+		, GCL_IMPLICIT_ALLOWED // Permit implicit values, so that we can say --server instead of --server=true
+		, true // Always randomly initialize if set
+	);
+
+	gpb.registerCLParameter<std::uint16_t>(
+		"nProducerThreads"
+		, nProducerThreads
+		, DEFAULTNPRODUCERTHREADS
+		, "The amount of random number producer threads"
+	);
+
+	gpb.registerCLParameter<std::uint16_t>(
+		"nEvaluationThreads"
+		, nEvaluationThreads
+		, DEFAULTNEVALUATIONTHREADS
+		, "The amount of threads processing individuals simultaneously in multi-threaded mode"
+	);
+
+	gpb.registerCLParameter<std::uint32_t>(
+		"maxIterations"
+		, maxIterations
+		, DEFAULTMAXITERATIONS
+		, "Maximum number of iterations in the optimization"
+	);
+
+	gpb.registerCLParameter<std::uint32_t>(
+		"reportIteration"
+		, reportIteration
+		, DEFAULTREPORTITERATION
+		, "The number of iterations after which information should be emitted in the population"
+	);
+
+	gpb.registerCLParameter<long>(
+		"maxMinutes"
+		, maxMinutes
+		, DEFAULTMAXMINUTES
+		, "The maximum number of minutes the optimization of the population should run"
+	);
+
+	gpb.registerCLParameter<std::uint16_t>(
+		"xDim"
+		, xDim
+		, DEFAULTXDIMAP
+		, "The x-dimension of the canvas for the result print(s)"
+	);
+
+	gpb.registerCLParameter<std::uint16_t>(
+		"yDim"
+		, yDim
+		, DEFAULTYDIMAP
+		, "The y-dimension of the canvas for the result print(s)"
+	);
+
+	gpb.registerCLParameter<bool>(
+		"followProgress"
+		, followProgress
+		, DEFAULTFOLLOWPROGRESS // Use client mode, if no server option is specified
+		, "Specifies whether snapshots should be taken in regular intervals. You can use this option with or without arguments."
+		, GCL_IMPLICIT_ALLOWED // Permit implicit values, so that we can say --server instead of --server=true
+		, true // Take snapshots, if --followProgress was specified without arguments
+	);
+
+
+	// Parse the command line and leave if the help flag was given. The parser
+	// will emit an appropriate help message by itself
+	if(Gem::Common::GCL_HELP_REQUESTED == gpb.parseCommandLine(argc, argv, true /*verbose*/)) {
+		return false; // Do not continue
+	}
+
+	return true;
+}
 
 /******************************************************************************/
 /**
