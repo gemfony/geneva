021ec7ee53bad188557a5ca6cbacba3bf9687c19
Ariel Garcia <a.garcia@gemfony.eu>
2020-02-02 14:37:27 +0100

    Update all source and header files for the conversion to the Apache License.

--- tests/geneva/ManualTests/GResetToOptimizationStartTest/GResetToOptimizationStart.cpp
+++ tests/geneva/ManualTests/GResetToOptimizationStartTest/GResetToOptimizationStart.cpp
@@ -2,35 +2,41 @@
  * @file GResetToOptimizationStart.cpp
  */
 
-/*
- * Copyright (C) Gemfony scientific UG (haftungsbeschraenkt)
+/********************************************************************************
  *
- * See the AUTHORS file in the top-level directory for a list of authors.
+ * This file is part of the Geneva library collection. The following license
+ * applies to this file:
  *
- * Contact: contact [at] gemfony (dot) eu
+ * ------------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file is part of the Geneva library collection.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Geneva was developed with kind support from Karlsruhe Institute of
- * Technology (KIT) and Steinbuch Centre for Computing (SCC). Further
- * information about KIT and SCC can be found at http://www.kit.edu/english
- * and http://scc.kit.edu .
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ------------------------------------------------------------------------------
  *
- * Geneva is free software: you can redistribute and/or modify it under
- * the terms of version 3 of the GNU Affero General Public License
- * as published by the Free Software Foundation.
+ * Note that other files in the Geneva library collection may use a different
+ * license. Please see the licensing information in each file.
  *
- * Geneva is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ ********************************************************************************
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with the Geneva library. If not, see <http://www.gnu.org/licenses/>.
+ * Geneva was started by Dr. Rüdiger Berlich and was later maintained together
+ * with Dr. Ariel Garcia under the auspices of Gemfony scientific. For further
+ * information on Gemfony scientific, see http://www.gemfomy.eu .
  *
- * For further information on Gemfony scientific and Geneva, visit
- * http://www.gemfony.eu .
- */
+ * The majority of files in Geneva was released under the Apache license v2.0
+ * in February 2020.
+ *
+ * See the NOTICE file in the top-level directory of the Geneva library
+ * collection for a list of contributors and copyright information.
+ *
+ ********************************************************************************/
 
 // Standard header files go here
 #include <iostream>
