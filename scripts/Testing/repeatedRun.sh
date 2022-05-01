3b891849af769db4c7bea2a682b8d2f43a822945
Jonas Weßner <jonas.wessner@gmx.de>
2022-05-01 16:25:02 +0200

    #12 create script to run command multiple times

---
+++ scripts/Testing/repeatedRun.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script repeatedly runs a command and saves all its output to a file.
+# This is useful if e.g. trying to statistically verify that a non-deterministic error is fixed.
+
+command=$1
+count=$2
+result_file=$3
+
+echo "running \`$command\` $count times and saving output to \`$result_file\`."
+
+# run many times and save stdout and stderr to a file
+for i in $(seq "$count"); do
+    echo "starting iteration $i..."
+    $command >> "$result_file" 2>&1
+done
+
+echo "finished all $count iterations."
