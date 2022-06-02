94c223f5ec534e9d904ce02c48a341b69f4b7d5f
Jonas Weßner <jonas.wessner@gmx.de>
2022-06-02 09:17:42 +0200

    #12 remove already resolved 'TODO', add new 'TODO'

--- include/courtier/GMPIConsumerT.hpp
+++ include/courtier/GMPIConsumerT.hpp
@@ -84,6 +84,8 @@
 
 // TODO: extract double buffering to GBaseConsumerClientT
 // TODO: extract docker setup to a separate branch
+// TODO: consider making GMPIConsumer only have server functionality and then offer a public method that returns a client
+//  this would be more consistent with the other consumers
 
 namespace Gem::Courtier {
     // constants that are used by the master and the worker nodes
