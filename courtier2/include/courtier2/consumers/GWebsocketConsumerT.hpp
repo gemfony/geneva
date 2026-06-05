3349c954375762b533d162fef06e10cf0e20133b
Ruediger <ruediger.berlich@gmail.com>
2026-06-05 16:28:20 +0200

    courtier2: websocket consumer (Phase 4, part 2)
    GWebsocketConsumerT: the websocket twin of the courtier2 ASIO consumer. Same
    per-batch checkout/checkin/dispatch_ machinery (inherited from
    GNetworkedConsumerT); a TCP server whose accepted connections are upgraded to
    websocket sessions with keep-alive ping/pong. Reuses the existing courtier
    GWebsocketConsumerSessionT and GCommandContainerT wire protocol, so an unmodified
    Gem::Courtier::Consumers::GWebsocketClientT serves a courtier2 server.
    
    GCourtier2WebsocketTests: 3 Tier-2 cases over real loopback websockets (clean
    batch / several clients sharing / throwing items refilled under
    clone-on-partial-return), all green.

---
+++ courtier2/include/courtier2/consumers/GWebsocketConsumerT.hpp
@@ -0,0 +1,227 @@
+/********************************************************************************
+ *
+ * This file is part of the Geneva library collection. The following license
+ * applies to this file:
+ *
+ * ------------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ------------------------------------------------------------------------------
+ *
+ * Note that other files in the Geneva library collection may use a different
+ * license. Please see the licensing information in each file.
+ *
+ ********************************************************************************
+ *
+ * See the NOTICE file in the top-level directory of the Geneva library
+ * collection for a list of contributors and copyright information.
+ *
+ ********************************************************************************/
+
+#pragma once
+
+#include "common/GGlobalDefines.hpp"
+
+// Standard headers
+#include <atomic>
+#include <cstddef>
+#include <memory>
+#include <thread>
+
+// Boost headers
+#include <boost/asio.hpp>
+
+// Geneva headers
+#include "common/GErrorStreamer.hpp"
+#include "common/GExceptions.hpp"
+#include "common/GLogger.hpp"
+#include "common/GThreadGroup.hpp"
+#include "courtier/consumers/GWebsocketConsumerT.hpp" // reuse the existing session + client + protocol
+#include "courtier2/consumers/GNetworkedConsumerT.hpp"
+
+namespace Gem::Courtier2 {
+
+/******************************************************************************/
+/**
+ * The courtier2 websocket consumer. Functionally the websocket twin of GAsioConsumerT: a TCP server
+ * whose accepted connections are upgraded to websocket sessions (with keep-alive ping/pong) that
+ * hand out work from the current batch and collect results, reconciled via the inherited
+ * GNetworkedConsumerT / GBaseConsumerT machinery.
+ *
+ * As with the ASIO consumer it reuses the existing courtier session
+ * (Gem::Courtier::Consumers::GWebsocketConsumerSessionT) and GCommandContainerT wire protocol, so an
+ * unmodified Gem::Courtier::Consumers::GWebsocketClientT serves a courtier2 server.
+ */
+template <typename processable_type>
+class GWebsocketConsumerT final
+  : public GNetworkedConsumerT<processable_type>
+  , public std::enable_shared_from_this<GWebsocketConsumerT<processable_type>> {
+public:
+    using session_type = Gem::Courtier::Consumers::GWebsocketConsumerSessionT<processable_type>;
+
+    /***************************************************************************/
+    explicit GWebsocketConsumerT(
+        unsigned short port,
+        std::size_t n_threads = 0,
+        Gem::Common::serializationMode serialization_mode = Gem::Common::serializationMode::BINARY,
+        std::size_t ping_interval = 5,
+        bool verbose_control_frames = false
+    )
+        : port_(port)
+        , n_threads_(n_threads == 0 ? default_threads() : n_threads)
+        , serialization_mode_(serialization_mode)
+        , ping_interval_(ping_interval)
+        , verbose_control_frames_(verbose_control_frames)
+    { /* nothing */ }
+
+    ~GWebsocketConsumerT() override { this->stopServer(); }
+
+    GWebsocketConsumerT(const GWebsocketConsumerT &) = delete;
+    GWebsocketConsumerT(GWebsocketConsumerT &&) = delete;
+    GWebsocketConsumerT &operator=(const GWebsocketConsumerT &) = delete;
+    GWebsocketConsumerT &operator=(GWebsocketConsumerT &&) = delete;
+
+    /***************************************************************************/
+    [[nodiscard]] unsigned short getPort() const noexcept { return port_; }
+    [[nodiscard]] std::size_t getNActiveSessions() const noexcept { return n_active_sessions_.load(); }
+
+    /***************************************************************************/
+    void startServer() {
+        boost::system::error_code ec;
+
+        boost::asio::ip::tcp::endpoint endpoint{boost::asio::ip::tcp::v4(), port_};
+        acceptor_.open(endpoint.protocol(), ec);
+        if(ec || not acceptor_.is_open()) {
+            throw geneva_exception(
+                g_error_streamer(DO_LOG, Gem::Common::timeAndPlace())
+                << "In Gem::Courtier2::GWebsocketConsumerT::startServer(): could not open the acceptor: "
+                << ec.message() << '\n'
+            );
+        }
+        acceptor_.set_option(boost::asio::socket_base::reuse_address(true));
+        acceptor_.bind(endpoint, ec);
+        if(ec) {
+            throw geneva_exception(
+                g_error_streamer(DO_LOG, Gem::Common::timeAndPlace())
+                << "In Gem::Courtier2::GWebsocketConsumerT::startServer(): could not bind to port "
+                << port_ << ": " << ec.message() << '\n'
+            );
+        }
+        port_ = acceptor_.local_endpoint().port();
+
+        acceptor_.listen(boost::asio::socket_base::max_listen_connections, ec);
+        if(ec) {
+            throw geneva_exception(
+                g_error_streamer(DO_LOG, Gem::Common::timeAndPlace())
+                << "In Gem::Courtier2::GWebsocketConsumerT::startServer(): could not listen: "
+                << ec.message() << '\n'
+            );
+        }
+
+        this->async_start_accept();
+
+        for(std::size_t t = 0; t < n_threads_; ++t) {
+            gtg_.create_thread([this] { io_context_.run(); });
+        }
+    }
+
+    /***************************************************************************/
+    void stopServer() {
+        if(stopped_already_.exchange(true)) {
+            return;
+        }
+        this->requestStop();
+
+        boost::system::error_code ec;
+        acceptor_.close(ec);
+
+        work_guard_.reset();
+        io_context_.stop();
+        gtg_.join_all();
+    }
+
+private:
+    /***************************************************************************/
+    static std::size_t default_threads() {
+        const unsigned int hc = std::thread::hardware_concurrency();
+        return hc == 0 ? 1u : hc;
+    }
+
+    /***************************************************************************/
+    void async_start_accept() {
+        auto self = this->shared_from_this();
+        acceptor_.async_accept(socket_, [self](boost::system::error_code ec) {
+            self->when_accepted(ec);
+        });
+    }
+
+    /***************************************************************************/
+    void when_accepted(boost::system::error_code ec) {
+        if(ec) {
+            if(not this->stopped()) {
+                glogger << "In Gem::Courtier2::GWebsocketConsumerT::when_accepted(): " << ec.message()
+                        << '\n'
+                        << GWARNING;
+            }
+        }
+        else {
+            std::make_shared<session_type>(
+                io_context_,
+                std::move(socket_),
+                [self = this->shared_from_this()]() -> std::shared_ptr<processable_type> {
+                    return self->checkout();
+                },
+                [self = this->shared_from_this()](std::shared_ptr<processable_type> p) {
+                    self->checkin(p);
+                },
+                [self = this->shared_from_this()]() -> bool { return self->stopped(); },
+                [self = this->shared_from_this()](bool sign_on) {
+                    if(sign_on) {
+                        ++self->n_active_sessions_;
+                    }
+                    else if(self->n_active_sessions_.load() > 0) {
+                        --self->n_active_sessions_;
+                    }
+                },
+                serialization_mode_,
+                ping_interval_,
+                verbose_control_frames_
+            )
+                ->async_start_run();
+        }
+
+        if(not this->stopped()) {
+            this->async_start_accept();
+        }
+    }
+
+    /***************************************************************************/
+    unsigned short port_;
+    std::size_t n_threads_;
+    Gem::Common::serializationMode serialization_mode_;
+    std::size_t ping_interval_;
+    bool verbose_control_frames_;
+
+    boost::asio::io_context io_context_;
+    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_ =
+        boost::asio::make_work_guard(io_context_);
+    boost::asio::ip::tcp::acceptor acceptor_{io_context_};
+    boost::asio::ip::tcp::socket socket_{io_context_};
+
+    Gem::Common::GThreadGroup gtg_;
+    std::atomic<std::size_t> n_active_sessions_{0};
+    std::atomic<bool> stopped_already_{false};
+};
+
+/******************************************************************************/
+
+} /* namespace Gem::Courtier2 */
